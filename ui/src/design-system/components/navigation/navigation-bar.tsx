1387b67be6d8519df83d037c3e528d58f1639d7c
Anna Viklund <annamariaviklund@gmail.com>
2023-03-10 09:41:11 +0100

    Setup navigation bar component and add to Storybook

---
+++ ui/src/design-system/components/navigation/navigation-bar.tsx
@@ -0,0 +1,67 @@
+import classNames from 'classnames'
+import { useCallback, useEffect, useState } from 'react'
+import styles from './navigation-bar.module.scss'
+
+interface NavigationBarProps {
+  activeItemId: string
+  items: { id: string; title: string; count?: number }[]
+  onItemClick: (id: string) => void
+}
+
+export const NavigationBar = ({
+  activeItemId,
+  items,
+  onItemClick,
+}: NavigationBarProps) => {
+  const [lineStyle, setLineStyle] = useState<React.CSSProperties>({})
+
+  const updateLineStyle = useCallback(() => {
+    const element = activeItemId && document.getElementById(activeItemId)
+    const updatedLineStyle = element
+      ? {
+          left: `${element.offsetLeft}px`,
+          width: `${element.offsetWidth}px`,
+        }
+      : {}
+    setLineStyle(updatedLineStyle)
+  }, [activeItemId])
+
+  // TODO: Pre load fonts and remove this work around.
+  useEffect(() => {
+    setTimeout(() => {
+      updateLineStyle()
+    }, 200)
+  }, [])
+
+  useEffect(() => updateLineStyle(), [activeItemId])
+
+  return (
+    <nav>
+      <ul className={styles.items}>
+        {items.map((item) => (
+          <li key={item.id} id={item.id}>
+            <div
+              role="button"
+              tabIndex={0}
+              className={classNames({
+                [styles.item]: true,
+                [styles.active]: activeItemId === item.id,
+              })}
+              onClick={() => onItemClick(item.id)}
+            >
+              <div className={styles.topContent}>
+                {item.count !== undefined && (
+                  <span className={styles.itemCount}>{item.count}</span>
+                )}
+              </div>
+              <div>
+                <span className={styles.itemTitle}>{item.title}</span>
+              </div>
+            </div>
+          </li>
+        ))}
+        <div className={styles.line} style={lineStyle} />
+      </ul>
+    </nav>
+  )
+}
