67e3a91924a94df3eda4e892eed62f5763e64199
Anna Viklund <annamariaviklund@gmail.com>
2023-03-29 20:23:41 +0200

    Extract gallery to a component

---
+++ ui/src/pages/occurrences/gallery/gallery.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames'
+import { Occurrence } from 'data-services/models/occurrence'
+import { Card } from 'design-system/components/card/card'
+import { LoadingSpinner } from 'design-system/components/loading-spinner/loading-spinner'
+import React from 'react'
+import styles from './gallery.module.scss'
+
+export const Gallery = ({
+  occurrences,
+  isLoading,
+}: {
+  occurrences: Occurrence[]
+  isLoading: boolean
+}) => {
+  return (
+    <div
+      className={classNames(styles.gallery, { [styles.loading]: isLoading })}
+    >
+      {occurrences.map((occurrence) => (
+        <div className={styles.cardWrapper}>
+          <Card
+            key={occurrence.id}
+            title={occurrence.categoryLabel}
+            subTitle="WIP"
+            image={occurrence.images[0]}
+            maxWidth="262px"
+          />
+        </div>
+      ))}
+      {isLoading && (
+        <div className={styles.loadingWrapper}>
+          <LoadingSpinner />
+        </div>
+      )}
+    </div>
+  )
+}
