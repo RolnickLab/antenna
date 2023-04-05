d4882a320271153f724cc1a335afca86dba72f7c
Anna Viklund <annamariaviklund@gmail.com>
2023-04-05 16:53:25 +0200

    Fix frame bug happening on first load

--- ui/src/pages/session-details/playback/frame/frame.tsx
+++ ui/src/pages/session-details/playback/frame/frame.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import { LoadingSpinner } from 'design-system/components/loading-spinner/loading-spinner'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react'
 import styles from './frame.module.scss'
 
 interface FrameProps {
@@ -14,7 +14,7 @@ export const Frame = ({ src, width, height, detections }: FrameProps) => {
   const imageRef = useRef<HTMLImageElement>(null)
   const [isLoading, setIsLoading] = useState<boolean>()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!imageRef.current) {
       return
     }
@@ -47,28 +47,30 @@ export const Frame = ({ src, width, height, detections }: FrameProps) => {
       style={{ paddingBottom: `${(height / width) * 100}%` }}
     >
       <img ref={imageRef} className={styles.image} />
-      <svg className={styles.overlay}>
-        <defs>
-          <mask id="holes">
-            <rect width="100%" height="100%" fill="white" />
-            {detections.map((detection) => {
-              const boxStyles = getBoxStyles(detection.bbox)
+      {!isLoading && (
+        <svg className={styles.overlay}>
+          <defs>
+            <mask id="holes">
+              <rect width="100%" height="100%" fill="white" />
+              {detections.map((detection) => {
+                const boxStyles = getBoxStyles(detection.bbox)
 
-              return (
-                <rect
-                  key={detection.id}
-                  x={boxStyles.left}
-                  y={boxStyles.top}
-                  width={boxStyles.width}
-                  height={boxStyles.height}
-                  fill="black"
-                />
-              )
-            })}
-          </mask>
-        </defs>
-        <rect fill="black" width="100%" height="100%" mask="url(#holes)" />
-      </svg>
+                return (
+                  <rect
+                    key={detection.id}
+                    x={boxStyles.left}
+                    y={boxStyles.top}
+                    width={boxStyles.width}
+                    height={boxStyles.height}
+                    fill="black"
+                  />
+                )
+              })}
+            </mask>
+          </defs>
+          <rect fill="black" width="100%" height="100%" mask="url(#holes)" />
+        </svg>
+      )}
       {isLoading && (
         <div className={styles.loadingWrapper}>
           <LoadingSpinner />
