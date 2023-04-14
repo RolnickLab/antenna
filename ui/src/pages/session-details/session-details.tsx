b0d9fe5c11101d852ae3536162f58a70eede47ef
Anna Viklund <annamariaviklund@gmail.com>
2023-04-14 14:17:08 +0200

    Add breadcrumbs (#125)

--- ui/src/pages/session-details/session-details.tsx
+++ ui/src/pages/session-details/session-details.tsx
@@ -1,14 +1,26 @@
 import { useSessionDetails } from 'data-services/hooks/useSessionDetails'
 import { LoadingSpinner } from 'design-system/components/loading-spinner/loading-spinner'
 import { Error } from 'pages/error/error'
-import { useParams } from 'react-router'
+import { useContext, useEffect } from 'react'
+import { useLocation, useParams } from 'react-router'
+import { BreadcrumbContext } from 'utils/breadcrumbContext'
 import { Playback } from './playback/playback'
 import styles from './session-details.module.scss'
 import { SessionInfo } from './session-info/session-info'
 
 export const SessionDetails = () => {
+  const location = useLocation()
   const { id } = useParams()
+  const { setDetailBreadcrumb } = useContext(BreadcrumbContext)
   const { session, isLoading, error } = useSessionDetails(id as string)
+
+  useEffect(() => {
+    setDetailBreadcrumb({ title: `Session #${id}`, path: location.pathname })
+
+    return () => {
+      setDetailBreadcrumb(undefined)
+    }
+  }, [location.pathname])
 
   if (isLoading) {
     return (
