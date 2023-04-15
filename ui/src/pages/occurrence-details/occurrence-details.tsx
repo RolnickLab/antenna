2a930e50a8a94e8d7e1e871fd05adca2bd6b9814
Anna Viklund <annamariaviklund@gmail.com>
2023-04-15 20:08:55 +0200

    Tweak rendering of images

--- ui/src/pages/occurrence-details/occurrence-details.tsx
+++ ui/src/pages/occurrence-details/occurrence-details.tsx
@@ -2,20 +2,15 @@ import { Occurrence } from 'data-services/models/occurrence'
 import { InfoBlock } from 'design-system/components/info-block/info-block'
 import * as Popover from 'design-system/components/popover/popover'
 import * as Tabs from 'design-system/components/tabs/tabs'
-import { Error } from 'pages/error/error'
 import { useState } from 'react'
 import { STRING, translate } from 'utils/language'
 import styles from './occurrence-details.module.scss'
 
-const mockData = {
+const taxonomyData = {
   taxonomy:
     'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Meropleon_diversicolor_-_Multicolored_Sedgeminer_Moth_%289810621354%29.jpg/440px-Meropleon_diversicolor_-_Multicolored_Sedgeminer_Moth_%289810621354%29.jpg',
   taxonomyDescription:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales cursus porta. Proin nec quam turpis.',
-  images: [
-    'https://api.dev.insectai.org/static/crops/43ee45ae97381dce5e9147e34f90b1c7.jpg',
-    'https://api.dev.insectai.org/static/crops/77d106412b6c81a6257a81e25abe59ea.jpg',
-  ],
 }
 
 export const OccurrenceDetails = ({
@@ -26,13 +21,7 @@ export const OccurrenceDetails = ({
   const [showTaxonomy, setShowTaxonomy] = useState(false)
 
   if (!occurrence) {
-    return (
-      <div className={styles.content}>
-        <div className={styles.info}>
-          <Error />
-        </div>
-      </div>
-    )
+    return null
   }
 
   const fields = [
@@ -56,68 +45,64 @@ export const OccurrenceDetails = ({
   ]
 
   return (
-    <div className={styles.content}>
-      <div className={styles.column}>
-        <div className={styles.header}>
-          <Popover.Root open={showTaxonomy} onOpenChange={setShowTaxonomy}>
-            <Popover.Trigger
-              onMouseEnter={() => setShowTaxonomy(true)}
-              onMouseLeave={() => setShowTaxonomy(false)}
-            >
-              <span className={styles.title}>{occurrence.categoryLabel}</span>
-            </Popover.Trigger>
-            <Popover.Content
-              ariaCloselabel={translate(STRING.CLOSE)}
-              align="start"
-              side="bottom"
-              hideClose
-            >
-              <div className={styles.taxonomyPopover}>
-                <img src={mockData.taxonomy} />
-                <div className={styles.taxonomyContent}>
-                  <p>{mockData.taxonomyDescription}</p>
-                </div>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <Popover.Root open={showTaxonomy} onOpenChange={setShowTaxonomy}>
+          <Popover.Trigger
+            onMouseEnter={() => setShowTaxonomy(true)}
+            onMouseLeave={() => setShowTaxonomy(false)}
+          >
+            <span className={styles.title}>{occurrence.categoryLabel}</span>
+          </Popover.Trigger>
+          <Popover.Content
+            ariaCloselabel={translate(STRING.CLOSE)}
+            align="start"
+            side="bottom"
+            hideClose
+          >
+            <div className={styles.taxonomyPopover}>
+              <img src={taxonomyData.taxonomy} />
+              <div className={styles.taxonomyContent}>
+                <p>{taxonomyData.taxonomyDescription}</p>
               </div>
-            </Popover.Content>
-          </Popover.Root>
-          <span className={styles.details}>WIP</span>
-        </div>
-        <div className={styles.info}>
-          <Tabs.Root defaultValue="fields">
-            <Tabs.List>
-              <Tabs.Trigger
-                value="fields"
-                label={translate(STRING.TAB_ITEM_FIELDS)}
-              />
-              <Tabs.Trigger
-                value="classification"
-                label={translate(STRING.TAB_ITEM_CLASSIFICATION)}
-              />
-            </Tabs.List>
-            <Tabs.Content value="fields">
-              <div className={styles.fields}>
-                <InfoBlock fields={fields} />
-              </div>
-            </Tabs.Content>
-            <Tabs.Content value="classification">
-              <div className={styles.fields}>
-                <InfoBlock fields={classifications} />
-              </div>
-            </Tabs.Content>
-          </Tabs.Root>
-        </div>
+            </div>
+          </Popover.Content>
+        </Popover.Root>
+        <span className={styles.details}>WIP</span>
       </div>
-      <div className={styles.column}>
+      <div className={styles.content}>
+        <div className={styles.column}>
+          <div className={styles.info}>
+            <Tabs.Root defaultValue="fields">
+              <Tabs.List>
+                <Tabs.Trigger
+                  value="fields"
+                  label={translate(STRING.TAB_ITEM_FIELDS)}
+                />
+                <Tabs.Trigger
+                  value="classification"
+                  label={translate(STRING.TAB_ITEM_CLASSIFICATION)}
+                />
+              </Tabs.List>
+              <Tabs.Content value="fields">
+                <div className={styles.fields}>
+                  <InfoBlock fields={fields} />
+                </div>
+              </Tabs.Content>
+              <Tabs.Content value="classification">
+                <div className={styles.fields}>
+                  <InfoBlock fields={classifications} />
+                </div>
+              </Tabs.Content>
+            </Tabs.Root>
+          </div>
+        </div>
         <div className={styles.images}>
-          {occurrence.images.slice(0, 2).map((image, index) => (
-            <div
-              key={index}
-              className={styles.imageWrapper}
-              style={{
-                backgroundImage: `url(${image.src})`,
-              }}
-            ></div>
-          ))}
+          <div className={styles.imagesContent}>
+            {occurrence.images.map((image, index) => (
+              <img alt="" src={image.src} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
