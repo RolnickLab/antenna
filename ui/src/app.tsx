9fbbdd99a3efa4ebb79158f7214a084e6fd9f473
Anna Viklund <annamariaviklund@gmail.com>
2023-05-04 15:50:34 +0200

    Setup React Query and React Query Devtools

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Breadcrumbs } from 'components/breadcrumbs'
 import { NavigationBar } from 'design-system/components/navigation/navigation-bar'
 import { BatchId } from 'pages/batch-id/batch-id'
@@ -14,67 +16,72 @@ import { BreadcrumbContextProvider } from 'utils/breadcrumbContext'
 import { useNavItems } from 'utils/useNavItems'
 import styles from './app.module.scss'
 
+const queryClient = new QueryClient()
+
 export const App = () => {
   const navigate = useNavigate()
   const { navItems, activeNavItemId } = useNavItems()
 
   return (
-    <BreadcrumbContextProvider>
-      <div className={styles.wrapper}>
-        <header className={styles.header}>
-          <div className={styles.topBar}>
-            <Breadcrumbs
-              navItems={navItems}
-              activeNavItemId={activeNavItemId}
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BreadcrumbContextProvider>
+        <div className={styles.wrapper}>
+          <header className={styles.header}>
+            <div className={styles.topBar}>
+              <Breadcrumbs
+                navItems={navItems}
+                activeNavItemId={activeNavItemId}
+              />
+              <Settings />
+            </div>
+            <NavigationBar
+              items={navItems}
+              activeItemId={activeNavItemId}
+              onItemClick={(id) => {
+                const item = navItems.find((i) => i.id === id)
+                if (item) {
+                  navigate(item.path)
+                }
+              }}
             />
-            <Settings />
-          </div>
-          <NavigationBar
-            items={navItems}
-            activeItemId={activeNavItemId}
-            onItemClick={(id) => {
-              const item = navItems.find((i) => i.id === id)
-              if (item) {
-                navigate(item.path)
-              }
-            }}
-          />
-        </header>
-        <main className={styles.content}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/overview" />} />
-            <Route path="/overview" element={<Overview />} />
-            <Route path="/batch-id" element={<BatchId />} />
-            <Route path="/deployments" element={<Deployments />} />
-            <Route path="/sessions" element={<Sessions />} />
-            <Route path="/sessions/:id" element={<SessionDetails />} />
-            <Route path="/occurrences/:id?" element={<Occurrences />} />
-            <Route path="/occurrences" element={<Occurrences />} />
-            <Route path="/species" element={<Species />} />
+          </header>
+          <main className={styles.content}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/overview" />} />
+              <Route path="/overview" element={<Overview />} />
+              <Route path="/batch-id" element={<BatchId />} />
+              <Route path="/deployments" element={<Deployments />} />
+              <Route path="/sessions" element={<Sessions />} />
+              <Route path="/sessions/:id" element={<SessionDetails />} />
+              <Route path="/occurrences/:id?" element={<Occurrences />} />
+              <Route path="/occurrences" element={<Occurrences />} />
+              <Route path="/species" element={<Species />} />
 
-            {/* Work in progress pages */}
-            <Route
-              path="/deployments/:id"
-              element={
-                <UnderConstruction message="Deployment details is under construction!" />
-              }
-            />
-            <Route
-              path="/occurrences/:id"
-              element={
-                <UnderConstruction message="Occurrence details is under construction!" />
-              }
-            />
-            <Route
-              path="/species/:id"
-              element={
-                <UnderConstruction message="Species details is under construction!" />
-              }
-            />
-            <Route path="*" element={<UnderConstruction />} />
-          </Routes>
-        </main>
-      </div>
-    </BreadcrumbContextProvider>
+              {/* Work in progress pages */}
+              <Route
+                path="/deployments/:id"
+                element={
+                  <UnderConstruction message="Deployment details is under construction!" />
+                }
+              />
+              <Route
+                path="/occurrences/:id"
+                element={
+                  <UnderConstruction message="Occurrence details is under construction!" />
+                }
+              />
+              <Route
+                path="/species/:id"
+                element={
+                  <UnderConstruction message="Species details is under construction!" />
+                }
+              />
+              <Route path="*" element={<UnderConstruction />} />
+            </Routes>
+          </main>
+        </div>
+      </BreadcrumbContextProvider>
+    </QueryClientProvider>
   )
 }
