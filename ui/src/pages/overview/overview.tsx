345a448c33dfe55813085794e9a6783134b3da20
Anna Viklund <annamariaviklund@gmail.com>
2023-05-12 16:59:41 +0200

    Add map to overview

--- ui/src/pages/overview/overview.tsx
+++ ui/src/pages/overview/overview.tsx
@@ -1,5 +1,9 @@
 import styles from './overview.module.scss'
 import { Plot } from 'design-system/components/plot/plot'
+import {
+  MultipleMarkerMap,
+  MarkerPosition,
+} from 'design-system/map/multiple-marker-map'
 
 const data = {
   y: [18, 45, 98, 120, 109, 113, 43],
@@ -7,12 +11,35 @@ const data = {
   tickvals: ['8PM', '', '', '', '', '', '14PM'],
 }
 
+const deployments = [
+  new MarkerPosition(52.30767, 5.04011),
+  new MarkerPosition(52.31767, 5.06011),
+  new MarkerPosition(52.32767, 5.09011),
+]
+
 export const Overview = () => {
   return (
     <>
       <div className={styles.section}>
         <div className={styles.content}>
-          <span className={styles.title}>Example plots</span>
+          <div className={styles.about}>
+            <div>
+              <span className={styles.title}>Example title</span>
+              <p className={styles.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur a sapien quis ligula suscipit tristique ullamcorper
+                porta tellus. Sed non quam lectus. Fusce nec elit ac tortor
+                viverra interdum quis at augue. Proin at lobortis ligula. Nullam
+                sed accumsan ex. Donec a pretium nisl.
+              </p>
+            </div>
+            <MultipleMarkerMap markerPositions={deployments} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.content}>
+          <span className={styles.label}>Example plots</span>
           <div className={styles.plots}>
             <Plot title="19 Jun" data={data} />
             <Plot title="20 Jun" data={data} type="scatter" />
