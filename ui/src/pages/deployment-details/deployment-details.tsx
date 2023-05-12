4a379e6b7ec9a040f39b50996e9f490c3de8526f
Anna Viklund <annamariaviklund@gmail.com>
2023-05-12 16:58:58 +0200

    Add map to deployment details

--- ui/src/pages/deployment-details/deployment-details.tsx
+++ ui/src/pages/deployment-details/deployment-details.tsx
@@ -1,6 +1,9 @@
 import { Deployment } from 'data-services/models/deployment'
 import * as Dialog from 'design-system/components/dialog/dialog'
-import { InputValue } from 'design-system/components/input/input'
+import { Input, InputValue } from 'design-system/components/input/input'
+import { Map, MarkerPosition } from 'design-system/map/map'
+import _ from 'lodash'
+import { useState } from 'react'
 import { STRING, translate } from 'utils/language'
 import styles from './deployment-details.module.scss'
 
@@ -44,10 +47,7 @@ const DeploymentDetails = ({ deployment }: { deployment?: Deployment }) => {
       <div className={styles.section}>
         <h2 className={styles.sectionTitle}>Location</h2>
         <div className={styles.sectionContent}>
-          <div className={styles.sectionRow}>
-            <InputValue label="Latitude" value="WIP" />
-            <InputValue label="Latitude" value="WIP" />
-          </div>
+          <DeploymentMap />
         </div>
       </div>
 
@@ -68,26 +68,70 @@ const DeploymentDetails = ({ deployment }: { deployment?: Deployment }) => {
           </div>
         </div>
       </div>
-
-      <div className={styles.section}>
-        <h2 className={styles.sectionTitle}>Processing</h2>
-        <div className={styles.sectionContent}>
-          <div className={styles.sectionRow}>
-            <InputValue label="Localization model" value="WIP" />
-            <InputValue label="Localization batch size" value="WIP" />
-          </div>
-          <div className={styles.sectionRow}>
-            <InputValue label="Binary classification model" value="WIP" />
-          </div>
-          <div className={styles.sectionRow}>
-            <InputValue label="Species classification model" value="WIP" />
-            <InputValue label="Classification batch size" value="WIP" />
-          </div>
-          <div className={styles.sectionRow}>
-            <InputValue label="Occurrence tracking algorithm" value="WIP" />
-          </div>
-        </div>
-      </div>
     </div>
+  )
+}
+
+const DeploymentMap = ({ editable }: { editable?: boolean }) => {
+  const markerPosition = new MarkerPosition(52.30767, 5.04011)
+
+  if (!editable) {
+    return (
+      <>
+        <div className={styles.sectionRow}>
+          <InputValue label="Latitude" value={markerPosition.lat} />
+          <InputValue label="Longitude" value={markerPosition.lng} />
+        </div>
+        <Map center={markerPosition} markerPosition={markerPosition} />
+      </>
+    )
+  }
+
+  return <EditableDeploymentMap defaultValue={markerPosition} />
+}
+
+const EditableDeploymentMap = ({
+  defaultValue,
+}: {
+  defaultValue: MarkerPosition
+}) => {
+  const [markerPosition, setMarkerPosition] = useState(defaultValue)
+  const [lat, setLat] = useState(markerPosition.lat)
+  const [lng, setLng] = useState(markerPosition.lng)
+
+  return (
+    <>
+      <div className={styles.sectionRow}>
+        <Input
+          name="latitude"
+          label="Latitude"
+          type="number"
+          value={lat}
+          onChange={(lat) => setLat(lat as number)}
+          onBlur={() => setMarkerPosition(new MarkerPosition(lat, lng))}
+        />
+        <Input
+          name="longitude"
+          label="Longitude"
+          type="number"
+          value={lng}
+          onChange={(lng) => setLng(lng as number)}
+          onBlur={() => setMarkerPosition(new MarkerPosition(lat, lng))}
+        />
+      </div>
+      <Map
+        center={markerPosition}
+        markerPosition={markerPosition}
+        markerDraggable
+        onMarkerPositionChange={(markerPosition) => {
+          const updatedLat = _.round(markerPosition.lat, 5)
+          const updatedLng = _.round(markerPosition.lng, 5)
+
+          setMarkerPosition(new MarkerPosition(updatedLat, updatedLng))
+          setLat(updatedLat)
+          setLng(updatedLng)
+        }}
+      />
+    </>
   )
 }
