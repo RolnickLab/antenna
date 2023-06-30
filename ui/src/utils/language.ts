4359908b25b663834bb05fb10dfed46e12829aa4
Michael Bunsen <notbot@gmail.com>
2023-06-29 17:52:25 -0700

    Change nav text to "Batch ID"

--- ui/src/utils/language.ts
+++ ui/src/utils/language.ts
@@ -161,7 +161,7 @@ const ENGLISH_STRINGS: { [key in STRING]: string } = {
 
   /* NAV_ITEM */
   [STRING.NAV_ITEM_DEPLOYMENTS]: 'Deployments',
-  [STRING.NAV_ITEM_JOBS]: 'Jobs',
+  [STRING.NAV_ITEM_JOBS]: 'Batch ID',
   [STRING.NAV_ITEM_OCCURRENCES]: 'Occurrences',
   [STRING.NAV_ITEM_OVERVIEW]: 'Overview',
   [STRING.NAV_ITEM_PROJECT]: 'Project',
