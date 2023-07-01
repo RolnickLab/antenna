7d3487c4872960b22dd0e6c7f31bc75c3a3c5fb4
Michael Bunsen <notbot@gmail.com>
2023-07-01 07:01:02 +0000

    Skip authentication for now

--- config/settings/base.py
+++ config/settings/base.py
@@ -325,7 +325,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
