ad9288d3686ea7ff27d7d1e829ac87a7a951f6cc
Anna Viklund <annamariaviklund@gmail.com>
2023-07-06 18:31:01 +0200

    Cleanup

--- ui/src/data-services/hooks/useInfiniteCaptures.ts
+++ ui/src/data-services/hooks/useInfiniteCaptures.ts
@@ -33,25 +33,24 @@ const fetchCaptures = async (sessionId: string, page: number) => {
 export const useInfiniteCaptures = (sessionId: string) => {
   const queryKey = [COLLECTION, { event: sessionId }]
 
-  const { data, hasNextPage, fetchNextPage, isFetching, isFetchingNextPage } =
-    useInfiniteQuery(
-      queryKey,
-      ({ pageParam = 1 }) => fetchCaptures(sessionId, pageParam - 1),
-      {
-        getNextPageParam: (lastPage, allPages) => {
-          const allItems = allPages.map((page) => page.results).flat(1)
+  const { data, hasNextPage, fetchNextPage, isFetching } = useInfiniteQuery(
+    queryKey,
+    ({ pageParam = 1 }) => fetchCaptures(sessionId, pageParam - 1),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const allItems = allPages.map((page) => page.results).flat(1)
 
-          if (
-            allItems.length >= lastPage.count ||
-            lastPage.results.length < PER_PAGE
-          ) {
-            return undefined
-          }
+        if (
+          allItems.length >= lastPage.count ||
+          lastPage.results.length < PER_PAGE
+        ) {
+          return undefined
+        }
 
-          return allPages.length + 1
-        },
-      }
-    )
+        return allPages.length + 1
+      },
+    }
+  )
 
   const captures = useMemo(
     () => data?.pages.map((page) => page.results).flat(1),
