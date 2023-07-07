9bb6dd2d531453351effabfbf8821a1b7b8b5c49
Anna Viklund <annamariaviklund@gmail.com>
2023-07-07 18:55:26 +0200

    Add pagination to jobs and bump deployments page size

--- ui/src/pages/jobs/jobs.tsx
+++ ui/src/pages/jobs/jobs.tsx
@@ -2,18 +2,21 @@ import { FetchInfo } from 'components/fetch-info/fetch-info'
 import { useJobDetails } from 'data-services/hooks/jobs/useJobDetails'
 import { useJobs } from 'data-services/hooks/jobs/useJobs'
 import * as Dialog from 'design-system/components/dialog/dialog'
+import { PaginationBar } from 'design-system/components/pagination/pagination-bar'
 import { Table } from 'design-system/components/table/table/table'
 import { Error } from 'pages/error/error'
 import { JobDetails } from 'pages/job-details/job-details'
 import { useNavigate, useParams } from 'react-router-dom'
 import { getRoute } from 'utils/getRoute'
 import { STRING, translate } from 'utils/language'
+import { usePagination } from 'utils/usePagination'
 import { columns } from './jobs-columns'
 import styles from './jobs.module.scss'
 
 export const Jobs = () => {
   const { id } = useParams()
-  const { jobs, isLoading, isFetching, error } = useJobs()
+  const { pagination, setPrevPage, setNextPage } = usePagination()
+  const { jobs, total, isLoading, isFetching, error } = useJobs({ pagination })
 
   if (!isLoading && error) {
     return <Error />
@@ -28,6 +31,15 @@ export const Jobs = () => {
       )}
       <Table items={jobs} isLoading={isLoading} columns={columns} />
       {!isLoading && id ? <JobDetailsDialog id={id} /> : null}
+      {jobs?.length ? (
+        <PaginationBar
+          page={pagination.page}
+          perPage={pagination.perPage}
+          total={total}
+          onPrevClick={setPrevPage}
+          onNextClick={setNextPage}
+        />
+      ) : null}
     </>
   )
 }
