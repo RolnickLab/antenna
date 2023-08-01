2178e32c23e15020dd98655ef8ea285621b0899f
Anna Viklund <annamariaviklund@gmail.com>
2023-08-01 20:40:16 +0200

    Add logo to header

--- ui/src/components/header/header.tsx
+++ ui/src/components/header/header.tsx
@@ -2,6 +2,7 @@ import { Breadcrumbs } from 'components/breadcrumbs/breadcrumbs'
 import { NavigationBar } from 'design-system/components/navigation/navigation-bar'
 import { useNavigate } from 'react-router-dom'
 import { useNavItems } from 'utils/useNavItems'
+import ami from './ami.png'
 import styles from './header.module.scss'
 
 export const Header = () => {
@@ -10,19 +11,24 @@ export const Header = () => {
 
   return (
     <header className={styles.header}>
+      <div className={styles.logoBar}>
+        <img src={ami} width={40} height={36} />
+      </div>
       <div className={styles.topBar}>
         <Breadcrumbs navItems={navItems} activeNavItemId={activeNavItemId} />
       </div>
-      <NavigationBar
-        items={navItems}
-        activeItemId={activeNavItemId}
-        onItemClick={(id) => {
-          const item = navItems.find((i) => i.id === id)
-          if (item) {
-            navigate(item.path)
-          }
-        }}
-      />
+      <div className={styles.navigationBar}>
+        <NavigationBar
+          items={navItems}
+          activeItemId={activeNavItemId}
+          onItemClick={(id) => {
+            const item = navItems.find((i) => i.id === id)
+            if (item) {
+              navigate(item.path)
+            }
+          }}
+        />
+      </div>
     </header>
   )
 }
