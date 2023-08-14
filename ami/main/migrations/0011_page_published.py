9c7164d11651fee852e253a01e0a747360727417
Michael Bunsen <notbot@gmail.com>
2023-08-14 15:54:09 -0700

    Merge page migrations

--- ami/main/migrations/0011_page_published.py
+++ ami/main/migrations/0011_page_published.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("main", "0007_page"),
+        ("main", "0010_page"),
     ]
 
     operations = [
