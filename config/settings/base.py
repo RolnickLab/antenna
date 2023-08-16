7d63cf3c327a203c77ca2bb0c330fbd345446d19
Michael Bunsen <notbot@gmail.com>
2023-08-15 18:25:52 -0700

    No global S3 config needed

--- config/settings/base.py
+++ config/settings/base.py
@@ -352,9 +352,3 @@ SPECTACULAR_SETTINGS = {
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
-
-
-# Storage defaults
-S3_ENDPOINT_URL = env("S3_ENDPOINT_URL", None)
-S3_ACCESS_KEY_ID = env("S3_ACCESS_KEY_ID", None)
-S3_SECRET_ACCESS_KEY = env("S3_SECRET_ACCESS_KEY", None)
