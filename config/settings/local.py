bc9c370bb6e07171629d64b760c5aea2543dd95e
Michael Bunsen <notbot@gmail.com>
2023-09-04 23:23:11 -0700

    Optimize postgres in docker, maybe?

--- config/settings/local.py
+++ config/settings/local.py
@@ -72,7 +72,7 @@ DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TEMPLATE_CONTEXT": True,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2", "*"]
 if env("USE_DOCKER") == "yes":
     import socket
 
