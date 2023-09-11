01f3691a335aa781df96bb00b1dc2a20f412c71d
Anna Viklund <annamariaviklund@gmail.com>
2023-09-11 14:40:07 +0200

    Add tests for hooks useAuthorizedQuery and useMe

---
+++ ui/src/data-services/hooks/auth/tests/useMe.test.ts
@@ -0,0 +1,56 @@
+import { renderHook, waitFor } from '@testing-library/react'
+import { API_URL } from 'data-services/constants'
+import nock from 'nock'
+import { AppMock, queryClient } from 'utils/test'
+import { AUTH_TOKEN_STORAGE_KEY } from 'utils/user/constants'
+import { useMe } from '../useMe'
+
+describe('useMe', () => {
+  test('will return user info on success', async () => {
+    // Prep
+    nock(API_URL)
+      .get('/auth/users/me/')
+      .reply(200, { email: 'user@insectai.org' })
+
+    // Run
+    const { result } = renderHook(() => useMe(), { wrapper: AppMock })
+    await waitFor(() => expect(result.current.user).toBeDefined())
+
+    // Check
+    expect(result.current.user).toEqual({
+      email: 'user@insectai.org',
+    })
+  })
+
+  test('will logout the user on 403 error', async () => {
+    // Prep
+    nock(API_URL).get('/auth/users/me/').times(2).reply(403)
+    localStorage.setItem(AUTH_TOKEN_STORAGE_KEY, 'example-token') // Simulate logged in user
+    const removeQueriesSpy = jest.spyOn(queryClient, 'removeQueries')
+
+    // Run
+    const { result } = renderHook(() => useMe(), { wrapper: AppMock })
+    await waitFor(() => expect(result.current.error).not.toBeNull())
+
+    // Check
+    expect(localStorage.getItem(AUTH_TOKEN_STORAGE_KEY)).toBeNull()
+    expect(removeQueriesSpy).toBeCalledTimes(1)
+  })
+
+  test('will keep the user logged in on error !== 403', async () => {
+    // Prep
+    nock(API_URL).get('/auth/users/me/').times(2).reply(500)
+    localStorage.setItem(AUTH_TOKEN_STORAGE_KEY, 'example-token') // Simulate logged in user
+    const removeQueriesSpy = jest.spyOn(queryClient, 'removeQueries')
+
+    // Run
+    const { result } = renderHook(() => useMe(), { wrapper: AppMock })
+    await waitFor(() => expect(result.current.error).not.toBeNull())
+
+    // Check
+    expect(localStorage.getItem(AUTH_TOKEN_STORAGE_KEY)).toEqual(
+      'example-token'
+    )
+    expect(removeQueriesSpy).toBeCalledTimes(0)
+  })
+})
--- ui/src/utils/user/userContext.test.tsx
+++ ui/src/utils/user/userContext.test.tsx
@@ -1,40 +1,7 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { act, renderHook } from '@testing-library/react'
-import { ReactNode } from 'react'
+import { AppMock, queryClient } from 'utils/test'
 import { AUTH_TOKEN_STORAGE_KEY } from './constants'
-import { UserContextProvider, useUser } from './userContext'
-
-// Mock local storage
-class LocalStorageMock {
-  store: { [key: string]: string } = {}
-
-  clear() {
-    this.store = {}
-  }
-
-  getItem(key: string) {
-    return this.store[key] || null
-  }
-
-  setItem(key: string, value: string) {
-    this.store[key] = String(value)
-  }
-
-  removeItem(key: string) {
-    delete this.store[key]
-  }
-}
-
-global.localStorage = new LocalStorageMock() as any
-
-// Mock app context
-const queryClient = new QueryClient()
-
-const wrapper = ({ children }: { children: ReactNode }) => (
-  <QueryClientProvider client={queryClient as any}>
-    <UserContextProvider>{children}</UserContextProvider>
-  </QueryClientProvider>
-)
+import { useUser } from './userContext'
 
 describe('useUser', () => {
   beforeEach(() => {
@@ -42,7 +9,7 @@ describe('useUser', () => {
   })
 
   test('will start as logged out as default', () => {
-    const { result } = renderHook(() => useUser(), { wrapper })
+    const { result } = renderHook(() => useUser(), { wrapper: AppMock })
 
     expect(result.current.user).toEqual({
       loggedIn: false,
@@ -52,7 +19,7 @@ describe('useUser', () => {
   test('will start as logged in, if local storage has token', () => {
     localStorage.setItem(AUTH_TOKEN_STORAGE_KEY, 'example-token')
 
-    const { result } = renderHook(() => useUser(), { wrapper })
+    const { result } = renderHook(() => useUser(), { wrapper: AppMock })
 
     expect(result.current.user).toEqual({
       loggedIn: true,
@@ -61,8 +28,7 @@ describe('useUser', () => {
   })
 
   test('will store token and change to logged in when token is set', () => {
-    const { result } = renderHook(() => useUser(), { wrapper })
-
+    const { result } = renderHook(() => useUser(), { wrapper: AppMock })
     act(() => {
       result.current.setToken('example-token')
     })
@@ -70,7 +36,6 @@ describe('useUser', () => {
     expect(localStorage.getItem(AUTH_TOKEN_STORAGE_KEY)).toEqual(
       'example-token'
     )
-
     expect(result.current.user).toEqual({
       loggedIn: true,
       token: 'example-token',
@@ -78,15 +43,13 @@ describe('useUser', () => {
   })
 
   test('will remove token from storage and change to logged out when token is cleared', () => {
-    const { result } = renderHook(() => useUser(), { wrapper })
-
+    const { result } = renderHook(() => useUser(), { wrapper: AppMock })
     act(() => {
       result.current.setToken('example-token')
       result.current.clearToken()
     })
 
     expect(localStorage.getItem(AUTH_TOKEN_STORAGE_KEY)).toBeNull()
-
     expect(result.current.user).toEqual({
       loggedIn: false,
       token: undefined,
@@ -94,26 +57,24 @@ describe('useUser', () => {
   })
 
   test('will remove queries after token is set', () => {
-    const spy = jest.spyOn(queryClient, 'removeQueries')
+    const removeQueriesSpy = jest.spyOn(queryClient, 'removeQueries')
 
-    const { result } = renderHook(() => useUser(), { wrapper })
-
+    const { result } = renderHook(() => useUser(), { wrapper: AppMock })
     act(() => {
       result.current.setToken('example-token')
     })
 
-    expect(spy).toBeCalledTimes(1)
+    expect(removeQueriesSpy).toBeCalledTimes(1)
   })
 
   test('will remove queries after token is cleared', () => {
-    const spy = jest.spyOn(queryClient, 'removeQueries')
+    const removeQueriesSpy = jest.spyOn(queryClient, 'removeQueries')
 
-    const { result } = renderHook(() => useUser(), { wrapper })
-
+    const { result } = renderHook(() => useUser(), { wrapper: AppMock })
     act(() => {
       result.current.clearToken()
     })
 
-    expect(spy).toBeCalledTimes(1)
+    expect(removeQueriesSpy).toBeCalledTimes(1)
   })
 })
