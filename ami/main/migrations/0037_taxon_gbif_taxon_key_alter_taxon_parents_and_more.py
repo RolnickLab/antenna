4256afb8b4c0ac81507e1d7371937b0f52084430
Michael Bunsen <notbot@gmail.com>
2023-09-14 17:57:13 -0700

    Merge migration ordering

--- ami/main/migrations/0037_taxon_gbif_taxon_key_alter_taxon_parents_and_more.py
+++ ami/main/migrations/0037_taxon_gbif_taxon_key_alter_taxon_parents_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("main", "0032_alter_deployment_options"),
+        ("main", "0036_alter_classification_options_and_more"),
     ]
 
     operations = [
