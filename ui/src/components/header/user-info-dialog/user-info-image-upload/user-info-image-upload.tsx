f71d74e5129789b267b5f7ca3ee460086405aae7
Anna Viklund <annamariaviklund@gmail.com>
2023-09-18 16:58:02 +0200

    Tweak profile image UI

--- ui/src/components/header/user-info-dialog/user-info-image-upload/user-info-image-upload.tsx
+++ ui/src/components/header/user-info-dialog/user-info-image-upload/user-info-image-upload.tsx
@@ -1,6 +1,6 @@
-import classNames from 'classnames'
 import { FileInput } from 'design-system/components/file-input/file-input'
 import { FileInputAccept } from 'design-system/components/file-input/types'
+import { useState } from 'react'
 import { UserInfo } from 'utils/user/types'
 import styles from './user-info-image-upload.module.scss'
 
@@ -13,6 +13,7 @@ export const UserInfoImageUpload = ({
   userInfo: UserInfo
   onChange: (file: File | null) => void
 }) => {
+  const [_size, setSize] = useState<{ width: number; height: number }>()
   const imageUrl = (() => {
     if (file) {
       return URL.createObjectURL(file)
@@ -24,22 +25,50 @@ export const UserInfoImageUpload = ({
   })()
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        {imageUrl ? <img src={imageUrl} /> : null}
-        <div
-          className={classNames(styles.overlay, {
-            [styles.hasImage]: !!imageUrl,
-          })}
-        >
-          <FileInput
-            accept={FileInputAccept.Images}
-            label="Choose image"
-            name="user-image"
-            onChange={onChange}
-          />
+    <>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          {imageUrl ? (
+            <>
+              <img
+                src={imageUrl}
+                onLoad={(e) => {
+                  const width = e.currentTarget.naturalWidth
+                  const height = e.currentTarget.naturalHeight
+                  setSize({ width, height })
+                }}
+              />
+              <ImageOverlay />
+            </>
+          ) : (
+            <span>No image</span>
+          )}
         </div>
       </div>
-    </div>
+      <FileInput
+        accept={FileInputAccept.Images}
+        label="Choose image"
+        name="user-image"
+        onChange={onChange}
+      />
+    </>
   )
 }
+
+const ImageOverlay = () => (
+  <svg className={styles.overlay}>
+    <defs>
+      <mask id="hole">
+        <rect width="100%" height="100%" fill="white" />
+        <circle cx="50%" cy="50%" r="50%" fill="black" />
+      </mask>
+    </defs>
+    <rect
+      fill="black"
+      fillOpacity={0.7}
+      width="100%"
+      height="100%"
+      mask="url(#hole)"
+    />
+  </svg>
+)
