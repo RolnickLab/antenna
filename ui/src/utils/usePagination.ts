308c576ff7988bf42d093dbe087735774ce1fe9a
Anna Viklund <annamariaviklund@gmail.com>
2023-10-30 14:17:10 +0100

    Consider search params in usePagination hook

--- ui/src/utils/usePagination.ts
+++ ui/src/utils/usePagination.ts
@@ -1,35 +1,60 @@
-import { TablePaginationSettings } from 'design-system/components/table/types'
-import { useCallback, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
-export const usePagination = ({
-  defaultPagination = { page: 0, perPage: 20 },
+const DEFAULT_PAGINATION = {
+  page: 0,
+  perPage: 20,
+}
+const SEARCH_PARAM_KEY_PAGE = 'page'
+const SEARCH_PARAM_KEY_PER_PAGE = 'per_page'
+
+const useSearchParam = ({
+  key,
+  defaultValue,
 }: {
-  defaultPagination?: TablePaginationSettings
-} = {}) => {
-  const [pagination, setPagination] =
-    useState<TablePaginationSettings>(defaultPagination)
+  key: string
+  defaultValue: number
+}) => {
+  const [searchParams, setSearchParams] = useSearchParams()
 
-  const setPrevPage = useCallback(
-    () =>
-      setPagination({
-        ...pagination,
-        page: pagination.page - 1,
-      }),
-    [pagination, setPagination]
-  )
+  const value = Number(searchParams.get(key) ?? defaultValue)
 
-  const setNextPage = useCallback(
-    () =>
-      setPagination({
-        ...pagination,
-        page: pagination.page + 1,
-      }),
-    [pagination, setPagination]
-  )
+  const setValue = (value: number) => {
+    const currentValue = searchParams.get(key)
+    const newValue = `${value}`
+
+    if (currentValue !== newValue) {
+      searchParams.delete(key)
+      searchParams.set(key, newValue)
+      setSearchParams(searchParams)
+    }
+  }
+
+  const clearValue = () => {
+    if (searchParams.has(key)) {
+      searchParams.delete(key)
+    }
+  }
+
+  return { value, setValue, clearValue }
+}
+
+export const usePagination = () => {
+  const { value: page, setValue: setPage } = useSearchParam({
+    key: SEARCH_PARAM_KEY_PAGE,
+    defaultValue: DEFAULT_PAGINATION.page,
+  })
+
+  const { value: perPage } = useSearchParam({
+    key: SEARCH_PARAM_KEY_PER_PAGE,
+    defaultValue: DEFAULT_PAGINATION.perPage,
+  })
 
   return {
-    pagination,
-    setPrevPage,
-    setNextPage,
+    pagination: {
+      page,
+      perPage,
+    },
+    setPrevPage: () => setPage(page - 1),
+    setNextPage: () => setPage(page + 1),
   }
 }
