d5dcfd8f8b48e29bfaa5359002418cadfb6befc6
Michael Bunsen <notbot@gmail.com>
2023-11-01 01:22:20 -0700

    Add scaffold for running jobs and celery tasks

--- ami/main/models.py
+++ ami/main/models.py
@@ -1893,10 +1893,22 @@ class BlogPost(BaseModel):
 
 
 # These come directly from Celery
-_JOB_STATES = ["PENDING", "STARTED", "SUCCESS", "FAILURE", "RETRY", "REVOKED", "RECEIVED"]
+_JOB_STATES = ["CREATED", "PENDING", "STARTED", "SUCCESS", "FAILURE", "RETRY", "REVOKED", "RECEIVED"]
 
 
 default_job_config = {
+    "stages": [
+        {
+            "name": "Delay test",
+            "key": "delay_test",
+            "params": [
+                {"key": "delay_seconds", "name": "Delay seconds", "value": 10},
+            ],
+        },
+    ]
+}
+
+default_ml_job_config = {
     "input": {
         "name": "Captures",
         "size": 100,
@@ -1982,7 +1994,23 @@ example_non_model_config = {
 }
 
 default_job_progress = {
-    "summary": {"status": "PENDING", "progress": 0, "status_label": "0% completed."},
+    "summary": {"status": "CREATED", "progress": 0, "status_label": "0% completed."},
+    "stages": [
+        {
+            "key": "delay_test",
+            "status": "PENDING",
+            "progress": 0,
+            "status_label": "0% completed.",
+            "time_elapsed": 0,
+            "time_remaining": None,
+            "input_size": 0,
+            "output_size": 0,
+        },
+    ],
+}
+
+default_ml_job_progress = {
+    "summary": {"status": "CREATED", "progress": 0, "status_label": "0% completed."},
     "stages": [
         {
             "key": "object_detection",
@@ -2039,15 +2067,80 @@ class Job(BaseModel):
     scheduled_at = models.DateTimeField(null=True, blank=True)
     started_at = models.DateTimeField(null=True, blank=True)
     finished_at = models.DateTimeField(null=True, blank=True)
-    status = models.CharField(max_length=255, default="PENDING", choices=as_choices(_JOB_STATES))
+    status = models.CharField(max_length=255, default="CREATED", choices=as_choices(_JOB_STATES))
     progress = models.JSONField(default=default_job_progress, null=True, blank=False)
     result = models.JSONField(null=True, blank=True)
+    task_id = models.CharField(max_length=255, null=True, blank=True)
 
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="jobs")
-    deployment = models.ForeignKey(Deployment, on_delete=models.CASCADE, related_name="jobs", null=True, blank=True)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="jobs",
+    )
+    deployment = models.ForeignKey(
+        Deployment,
+        on_delete=models.CASCADE,
+        related_name="jobs",
+        null=True,
+        blank=True,
+    )
+    source_image_single = models.ForeignKey(
+        SourceImage,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="jobs",
+    )
+    source_image_collection = models.ForeignKey(
+        "SourceImageCollection",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="jobs",
+    )
 
     def __str__(self) -> str:
         return f"{self.name} ({self.status})"
+
+    def enqueue(self):
+        """
+        Add the job to the queue so that it will run in the background.
+        """
+        # ami.tasks.run_job.delay(self.pk)
+        # task_id = ami.tasks.run_job.apply_async(args=[self.pk], queue=self.queue).id
+        task_id = ami.tasks.run_job.apply_async(args=[self.pk]).id
+        self.task_id = task_id
+        self.started_at = None
+        self.finished_at = None
+        self.scheduled_at = datetime.datetime.now()
+        self.status = ami.tasks.run_job.AsyncResult(task_id).status
+        self.save()
+
+    def run(self):
+        """
+        Run the job.
+
+        This is meant to be called by an async task, not directly.
+        """
+
+        self.status = "STARTED"
+        self.started_at = datetime.datetime.now()
+        self.finished_at = None
+        self.save()
+
+        # check if there is a delay seconds configured
+        delay_seconds = 0
+        config = self.config or {}
+        for stage in config.get("stages", []):
+            if stage.get("key") == "delay_test":
+                for param in stage.get("params", []):
+                    if param.get("key") == "delay_seconds":
+                        delay_seconds = param.get("value", 0)
+        time.sleep(delay_seconds)
+
+        self.status = "SUCCESS"
+        self.finished_at = datetime.datetime.now()
+        self.save()
 
     @classmethod
     def default_config(cls) -> dict:
