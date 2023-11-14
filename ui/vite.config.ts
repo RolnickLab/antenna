744394f6d11bb83f351598a8cb943758113f9988
Anna Viklund <annamariaviklund@gmail.com>
2023-11-14 13:58:21 +0100

    Handle SVG imports in a different way

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   base: '/',
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths(), svgr({ include: '**/*.svg?react' })],
   server: {
     open: true,
     port: 3000,
