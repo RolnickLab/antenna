59f489916d5d39586f109d10adfeb4310e6a2cfe
Anna Viklund <annamariaviklund@gmail.com>
2023-11-14 11:39:57 +0100

    Update config files and setup new proxy

---
+++ ui/vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  base: '/',
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://api.dev.insectai.org',
+        changeOrigin: true,
+      },
+    },
+  },
+})
