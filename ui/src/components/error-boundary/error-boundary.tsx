50f952031890b92ee77c9f9ac83d8254c3f0d9d1
Anna Viklund <annamariaviklund@gmail.com>
2023-11-23 10:09:30 +0100

    Log errors with Sentry

--- ui/src/components/error-boundary/error-boundary.tsx
+++ ui/src/components/error-boundary/error-boundary.tsx
@@ -1,14 +1,17 @@
+import * as Sentry from '@sentry/browser'
 import { Button } from 'design-system/components/button/button'
 import { Icon, IconTheme, IconType } from 'design-system/components/icon/icon'
 import { Tooltip } from 'design-system/components/tooltip/tooltip'
-import { ErrorInfo, ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { ErrorBoundary as _ErrorBoundary } from 'react-error-boundary'
 import { STRING, translate } from 'utils/language'
 import styles from './error-boundary.module.scss'
 
-const logErrorToService = (error: Error, info: ErrorInfo) => {
-  // TODO: Pass error to Sentry here
-  console.error(error, info)
+const logErrorToService = (error: Error, errorInfo: any) => {
+  Sentry.withScope((scope) => {
+    scope.setExtras(errorInfo)
+    Sentry.captureException(error)
+  })
 }
 
 const FallbackComponent = ({
