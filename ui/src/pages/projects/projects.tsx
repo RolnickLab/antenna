32da7fb7fbbe61e1651fbaecce9278a3ee980f8b
Anna Viklund <annamariaviklund@gmail.com>
2024-06-07 10:37:03 +0200

    Add pagination to projects

--- ui/src/pages/projects/projects.tsx
+++ ui/src/pages/projects/projects.tsx
@@ -1,15 +1,18 @@
 import { FetchInfo } from 'components/fetch-info/fetch-info'
 import { useProjects } from 'data-services/hooks/projects/useProjects'
+import { PaginationBar } from 'design-system/components/pagination-bar/pagination-bar'
 import { Error } from 'pages/error/error'
 import { NewProjectDialog } from 'pages/project-details/new-project-dialog'
 import { STRING, translate } from 'utils/language'
+import { usePagination } from 'utils/usePagination'
 import { UserPermission } from 'utils/user/types'
 import { ProjectGallery } from './project-gallery'
 import styles from './projects.module.scss'
 
 export const Projects = () => {
-  const { projects, userPermissions, isLoading, isFetching, error } =
-    useProjects()
+  const { pagination, setPage } = usePagination({ perPage: 24 })
+  const { projects, total, userPermissions, isLoading, isFetching, error } =
+    useProjects({ pagination })
 
   if (!isLoading && error) {
     return <Error />
@@ -28,6 +31,13 @@ export const Projects = () => {
       <div className={styles.divider} />
       <ProjectGallery projects={projects} isLoading={isLoading} />
       <div className={styles.spacer} />
+      {projects?.length ? (
+        <PaginationBar
+          pagination={pagination}
+          total={total}
+          setPage={setPage}
+        />
+      ) : null}
       {canCreate && <NewProjectDialog />}
     </>
   )
