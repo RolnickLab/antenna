ac8322011f14c9d652a126469c67c2968d527a60
Anna Viklund <annamariaviklund@gmail.com>
2024-06-12 09:39:26 +0200

    Conditionally render algorithm tooltip

--- ui/src/pages/occurrence-details/algorithm-details/algorithm-details.tsx
+++ ui/src/pages/occurrence-details/algorithm-details/algorithm-details.tsx
@@ -2,8 +2,14 @@ import { Algorithm } from 'data-services/models/algorithm'
 import { Tooltip } from 'design-system/components/tooltip/tooltip'
 import styles from './algorithm-details.module.scss'
 
-export const AlgorithmDetails = ({ algorithm }: { algorithm: Algorithm }) => (
-  <Tooltip content={algorithm.description}>
-    <div className={styles.algorithmDetails}>{algorithm.name}</div>
-  </Tooltip>
-)
+export const AlgorithmDetails = ({ algorithm }: { algorithm: Algorithm }) => {
+  if (!algorithm.description) {
+    return <div className={styles.algorithmDetails}>{algorithm.name}</div>
+  }
+
+  return (
+    <Tooltip content={algorithm.description}>
+      <div className={styles.algorithmDetails}>{algorithm.name}</div>
+    </Tooltip>
+  )
+}
