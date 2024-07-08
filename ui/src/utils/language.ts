0ee73aaa9f6377b9e6c306478da242aa2279de8e
Anna Viklund <annamariaviklund@gmail.com>
2024-07-08 15:48:35 +0200

    Update "Status" -> "Jobs"

--- ui/src/utils/language.ts
+++ ui/src/utils/language.ts
@@ -342,7 +342,7 @@ const ENGLISH_STRINGS: { [key in STRING]: string } = {
 
   /* NAV_ITEM */
   [STRING.NAV_ITEM_DEPLOYMENTS]: 'Deployments',
-  [STRING.NAV_ITEM_JOBS]: 'Status',
+  [STRING.NAV_ITEM_JOBS]: 'Jobs',
   [STRING.NAV_ITEM_OCCURRENCES]: 'Occurrences',
   [STRING.NAV_ITEM_OVERVIEW]: 'Overview',
   [STRING.NAV_ITEM_PROJECTS]: 'Projects',
