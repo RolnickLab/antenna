efb1fa63abfc5b36d4c7c05d633a5d018fb23f92
Michael Bunsen <notbot@gmail.com>
2024-08-27 22:34:53 -0700

    feat: move ml_backends to its own compose file

---
+++ ml_backends/example/api/api.py
@@ -0,0 +1,47 @@
+"""
+Fast API interface for processing images through the localization and classification pipelines.
+"""
+
+import time
+
+import fastapi
+
+from .pipeline import DummyPipeline
+from .schemas import PipelineRequest, PipelineResponse, SourceImage, SourceImageResponse
+
+app = fastapi.FastAPI()
+
+# Make slash and no slash endpoints work:
+
+
+@app.get("/")
+async def root():
+    return fastapi.responses.RedirectResponse("/docs")
+
+
+@app.post("/pipeline/process/")
+async def process(data: PipelineRequest) -> PipelineResponse:
+    source_image_results = [SourceImageResponse(**image.model_dump()) for image in data.source_images]
+    source_images = [SourceImage(**image.model_dump()) for image in data.source_images]
+
+    start_time = time.time()
+
+    pipeline = DummyPipeline(source_images=source_images)
+    results = pipeline.run()
+
+    end_time = time.time()
+    seconds_elapsed = float(end_time - start_time)
+
+    response = PipelineResponse(
+        pipeline=data.pipeline,
+        source_images=source_image_results,
+        detections=results,
+        total_time=seconds_elapsed,
+    )
+    return response
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=2000)
