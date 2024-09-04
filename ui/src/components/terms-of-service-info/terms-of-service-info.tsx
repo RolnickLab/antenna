5230496873d0296b42fe2fecfc743eb823b4fd71
Anna Viklund <annamariaviklund@gmail.com>
2024-09-04 15:40:43 +0200

    fix: mark message as seen when component unmounts

--- ui/src/components/terms-of-service-info/terms-of-service-info.tsx
+++ ui/src/components/terms-of-service-info/terms-of-service-info.tsx
@@ -3,6 +3,7 @@ import {
   IconButtonTheme,
 } from 'design-system/components/icon-button/icon-button'
 import { IconType } from 'design-system/components/icon/icon'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { APP_ROUTES } from 'utils/constants'
 import { useUserPreferences } from 'utils/userPreferences/userPreferencesContext'
@@ -10,6 +11,13 @@ import styles from './terms-of-service-info.module.scss'
 
 export const TermsOfServiceInfo = () => {
   const { userPreferences, setUserPreferences } = useUserPreferences()
+
+  useEffect(() => {
+    return () => {
+      // Mark message as seen when component unmounts
+      setUserPreferences({ ...userPreferences, termsMessageSeen: true })
+    }
+  }, [])
 
   if (userPreferences.termsMessageSeen) {
     return null
