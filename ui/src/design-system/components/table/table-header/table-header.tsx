642c90b10da7cd40cf5a9019d409ccf5d333f5ed
Anna Viklund <annamariaviklund@gmail.com>
2024-09-06 14:48:03 +0200

    feat: make it possible to show extra info in table headers

--- ui/src/design-system/components/table/table-header/table-header.tsx
+++ ui/src/design-system/components/table/table-header/table-header.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { Icon, IconTheme, IconType } from 'design-system/components/icon/icon'
+import { Tooltip } from 'design-system/components/tooltip/tooltip'
 import { TableColumn, TableSortSettings } from '../types'
 import styles from './table-header.module.scss'
 
@@ -36,22 +37,32 @@ const BasicTableHeader = <T,>({
   column,
   visuallyHidden,
 }: Omit<TableHeaderProps<T>, 'sortSettings' | 'onSortClick'>) => (
-  <th
-    key={column.id}
-    style={{ textAlign: column.styles?.textAlign, width: column.styles?.width }}
-    className={styles.tableHeader}
-  >
-    <div
-      className={classNames(styles.content, {
-        [styles.visuallyHidden]: visuallyHidden,
-      })}
-      style={{ padding: column.styles?.padding }}
+  <Tooltip content={column.tooltip}>
+    <th
+      key={column.id}
+      style={{
+        textAlign: column.styles?.textAlign,
+        width: column.styles?.width,
+      }}
+      className={styles.tableHeader}
     >
-      <div className={styles.columnName}>
-        <span>{column.name}</span>
+      <div
+        className={classNames(styles.content, {
+          [styles.visuallyHidden]: visuallyHidden,
+        })}
+        style={{ padding: column.styles?.padding }}
+      >
+        <div className={styles.columnName}>
+          <span>{column.name}</span>
+          {column.tooltip ? (
+            <div className={styles.iconWrapper}>
+              <Icon type={IconType.Info} theme={IconTheme.Neutral} />
+            </div>
+          ) : null}
+        </div>
       </div>
-    </div>
-  </th>
+    </th>
+  </Tooltip>
 )
 
 const SortableTableHeader = <T,>({
@@ -78,25 +89,34 @@ const SortableTableHeader = <T,>({
         [styles.active]: sortActive,
       })}
     >
-      <button
-        className={classNames(styles.content, styles.sortButton, {
-          [styles.visuallyHidden]: visuallyHidden,
-        })}
-        style={{ padding: column.styles?.padding }}
-        onClick={onSortClick}
-      >
-        <div className={styles.columnName}>
-          <span>{column.name}</span>
-          <div
-            className={classNames(styles.iconWrapper, {
-              [styles.visible]: sortActive,
-              [styles.ascending]: sortActive && sortSettings?.order === 'asc',
-            })}
-          >
-            <Icon type={IconType.Sort} theme={IconTheme.Neutral} />
+      <Tooltip content={column.tooltip}>
+        <button
+          className={classNames(styles.content, styles.sortButton, {
+            [styles.visuallyHidden]: visuallyHidden,
+          })}
+          style={{ padding: column.styles?.padding }}
+          onClick={onSortClick}
+        >
+          <div className={styles.columnName}>
+            <span>{column.name}</span>
+            {column.tooltip && !sortActive ? (
+              <div className={styles.iconWrapper}>
+                <Icon type={IconType.Info} theme={IconTheme.Neutral} />
+              </div>
+            ) : null}
+            {sortActive ? (
+              <div
+                className={classNames(styles.iconWrapper, {
+                  [styles.ascending]:
+                    sortActive && sortSettings?.order === 'asc',
+                })}
+              >
+                <Icon type={IconType.Sort} theme={IconTheme.Neutral} />
+              </div>
+            ) : null}
           </div>
-        </div>
-      </button>
+        </button>
+      </Tooltip>
     </th>
   )
 }
