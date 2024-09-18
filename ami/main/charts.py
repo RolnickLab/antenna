1ed5be8145489baf7c3a216decbd48e82370b4e4
Vanessa Mac <vanessavaleriemac@gmail.com>
2024-09-18 09:50:29 -0400

    Fix order

--- ami/main/charts.py
+++ ami/main/charts.py
@@ -269,11 +269,11 @@ def event_top_taxa(event_pk: int, top_n: int = 10):
         .values("name")
         # .annotate(num_detections=models.Count("occurrences__detections"))
         .annotate(num_detections=models.Count("occurrences"))
-        .order_by("num_detections")[:top_n]
+        .order_by("-num_detections")[:top_n]
     )
 
     if top_taxa:
-        taxa, counts = list(zip(*[(t["name"], t["num_detections"]) for t in top_taxa]))
+        taxa, counts = list(zip(*[(t["name"], t["num_detections"]) for t in reversed(top_taxa)]))
         taxa = [t or "Unknown" for t in taxa]
         counts = [c or 0 for c in counts]
     else:
