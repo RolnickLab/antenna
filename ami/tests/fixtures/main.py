e6e2a58f87406be62d41b45a96be21b1b280efa7
Vanessa Mac <vanessavaleriemac@gmail.com>
2024-10-11 12:59:19 -0400

    Replace management command with test project set up

--- ami/tests/fixtures/main.py
+++ ami/tests/fixtures/main.py
@@ -33,6 +33,45 @@ def update_site_settings(**kwargs):
     return site
 
 
+def create_ml_pipeline(project):
+    from ami.ml.models import Algorithm, Pipeline
+
+    pipelines_to_add = [
+        {
+            "name": "ML Dummy Backend",
+            "slug": "dummy",
+            "version": 1,
+            "algorithms": [
+                {"name": "Dummy Detector", "key": 1},
+                {"name": "Random Detector", "key": 2},
+                {"name": "Always Moth Classifier", "key": 3},
+            ],
+            "projects": {"name": project.name},
+            # @TODO Replace with extra hosts
+            "endpoint_url": "https://03f9-24-114-29-178.ngrok-free.app/pipeline/process",
+        },
+    ]
+
+    for pipeline_data in pipelines_to_add:
+        pipeline, created = Pipeline.objects.get_or_create(
+            name=pipeline_data["name"],
+            slug=pipeline_data["slug"],
+            version=pipeline_data["version"],
+            endpoint_url=pipeline_data["endpoint_url"],
+        )
+
+        if created:
+            logger.info(f'Successfully created {pipeline_data["name"]}.')
+        else:
+            logger.warning(f'Could not create {pipeline_data["name"]}.')
+
+        for algorithm_data in pipeline_data["algorithms"]:
+            algorithm, _ = Algorithm.objects.get_or_create(name=algorithm_data["name"], key=algorithm_data["key"])
+            pipeline.algorithms.add(algorithm)
+
+        pipeline.save()
+
+
 def setup_test_project(reuse=True) -> tuple[Project, Deployment]:
     if reuse:
         project, _ = Project.objects.get_or_create(name="Test Project")
@@ -40,6 +79,7 @@ def setup_test_project(reuse=True) -> tuple[Project, Deployment]:
         deployment, _ = Deployment.objects.get_or_create(
             project=project, name="Test Deployment", defaults=dict(data_source=data_source)
         )
+        create_ml_pipeline(project)
     else:
         short_id = uuid.uuid4().hex[:8]
         project = Project.objects.create(name=f"Test Project {short_id}")
@@ -47,6 +87,7 @@ def setup_test_project(reuse=True) -> tuple[Project, Deployment]:
         deployment = Deployment.objects.create(
             project=project, name=f"Test Deployment {short_id}", data_source=data_source
         )
+        create_ml_pipeline(project)
     return project, deployment
 
 
