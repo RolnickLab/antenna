12990f2f87d0df8b725a16515c3780152bdb7cb4
Anna Viklund <annamariaviklund@gmail.com>
2024-10-25 16:59:34 +0200

    Setup score threshold filter for list views (#603)
    * feat: use score threshold filter in occurrence list view
    
    * feat: use score threshold filter in species list view

--- ui/src/utils/useFilters.ts
+++ ui/src/utils/useFilters.ts
@@ -22,6 +22,10 @@ const AVAILABLE_FILTERS = [
     field: 'taxon',
   },
   {
+    label: 'Score threshold',
+    field: 'classification_threshold',
+  },
+  {
     label: 'Capture collection',
     field: 'collection',
   },
@@ -31,15 +35,20 @@ const AVAILABLE_FILTERS = [
   },
 ]
 
-export const useFilters = () => {
+export const useFilters = (
+  defaultFilters?: { field: string; value: string }[]
+) => {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const filters = AVAILABLE_FILTERS.map((filter) => {
-    const values = searchParams.getAll(filter.field)
+    const value = searchParams.getAll(filter.field)[0]
+    const defaultValue = defaultFilters?.find(
+      (defaultFilter) => defaultFilter.field === filter.field
+    )?.value
 
     return {
       ...filter,
-      value: values[0],
+      value: value ?? defaultValue,
     }
   })
 
