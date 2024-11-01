211d45c67ff54a3982c03f41e921a2b3533aaece
Anna Viklund <annamariaviklund@gmail.com>
2024-11-01 17:27:46 +0100

    style: configure Tailwind

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,8 +1,14 @@
+import { CONSTANTS } from 'nova-ui-kit'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx}', './node_modules/nova-ui-kit/**/*.js'],
   theme: {
-    extend: {},
+    colors: CONSTANTS.COLORS,
+    extend: {
+      backgroundImage: CONSTANTS.GRADIENTS,
+      colors: CONSTANTS.COLOR_THEME,
+    },
   },
   plugins: [],
 }
