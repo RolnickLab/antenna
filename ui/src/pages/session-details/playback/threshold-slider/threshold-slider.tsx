72f93e565c5ee9dc597b26070e856ad9ddd76d5f
Anna Viklund <annamariaviklund@gmail.com>
2024-11-19 17:13:49 +0100

    chore: refactor threshold slider component

--- ui/src/pages/session-details/playback/threshold-slider/threshold-slider.tsx
+++ ui/src/pages/session-details/playback/threshold-slider/threshold-slider.tsx
@@ -1,27 +1,46 @@
-import { ScoreSlider } from 'design-system/components/slider/score-slider'
+import { Slider } from 'nova-ui-kit'
 import { useState } from 'react'
 import { STRING, translate } from 'utils/language'
 import { useUserPreferences } from 'utils/userPreferences/userPreferencesContext'
 
 export const ThresholdSlider = () => {
+  const [active, setActive] = useState(false)
   const { userPreferences, setUserPreferences } = useUserPreferences()
   const [displayThreshold, setDisplayThreshold] = useState(
     userPreferences.scoreThreshold
   )
 
+  const onValueCommit = (value: number) => {
+    setDisplayThreshold(value)
+    setUserPreferences({
+      ...userPreferences,
+      scoreThreshold: value,
+    })
+  }
+
   return (
-    <ScoreSlider
-      defaultValue={userPreferences.scoreThreshold}
-      label={translate(STRING.FIELD_LABEL_SCORE)}
-      value={displayThreshold}
-      onValueChange={setDisplayThreshold}
-      onValueCommit={(value) => {
-        setDisplayThreshold(value)
-        setUserPreferences({
-          ...userPreferences,
-          scoreThreshold: value,
-        })
-      }}
-    />
+    <div className="w-full h-12 flex items-center gap-4 text-generic-white">
+      <span className="body-overline-small font-bold">
+        {translate(STRING.FIELD_LABEL_SCORE)}
+      </span>
+      <Slider
+        invertedColors
+        defaultValue={[userPreferences.scoreThreshold]}
+        min={0}
+        max={1}
+        step={0.01}
+        value={[displayThreshold]}
+        onValueChange={([value]) => setDisplayThreshold(value)}
+        onValueCommit={([value]) => onValueCommit(value)}
+        onPointerDown={() => setActive(true)}
+        onPointerUp={() => setActive(false)}
+        onPointerLeave={() => {
+          if (active) {
+            onValueCommit(displayThreshold)
+          }
+        }}
+      />
+      <span className="w-12 text-right body-overline">{displayThreshold}</span>
+    </div>
   )
 }
