2b0f03947ef650353cfd186d204a5ddec415a694
Michael Bunsen <notbot@gmail.com>
2024-12-04 20:53:38 -0800

    feat: API views for algorithm category maps

--- ami/ml/views.py
+++ ami/ml/views.py
@@ -1,8 +1,8 @@
 from ami.main.api.views import DefaultViewSet
 
-from .models.algorithm import Algorithm
+from .models.algorithm import Algorithm, AlgorithmCategoryMap
 from .models.pipeline import Pipeline
-from .serializers import AlgorithmSerializer, PipelineSerializer
+from .serializers import AlgorithmCategoryMapSerializer, AlgorithmSerializer, PipelineSerializer
 
 
 class AlgorithmViewSet(DefaultViewSet):
@@ -20,6 +20,22 @@ class AlgorithmViewSet(DefaultViewSet):
         "version",
     ]
     search_fields = ["name"]
+
+
+class AlgorithmCategoryMapViewSet(DefaultViewSet):
+    """
+    API endpoint that allows algorithm category maps to be viewed or edited.
+    """
+
+    queryset = AlgorithmCategoryMap.objects.all()
+    serializer_class = AlgorithmCategoryMapSerializer
+    filterset_fields = ["algorithms"]
+    ordering_fields = [
+        "algorithms",
+        "created_at",
+        "updated_at",
+        "version",
+    ]
 
 
 class PipelineViewSet(DefaultViewSet):
