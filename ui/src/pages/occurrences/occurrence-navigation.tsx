9ed1bdcd9cf938c5bb7eb77996cba97b54cb0596
Anna Viklund <annamariaviklund@gmail.com>
2024-12-17 15:00:13 +0100

    feat: add keyboard quick actions for occurrence navigation

--- ui/src/pages/occurrences/occurrence-navigation.tsx
+++ ui/src/pages/occurrences/occurrence-navigation.tsx
@@ -1,6 +1,7 @@
 import { Occurrence } from 'data-services/models/occurrence'
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 import { Button } from 'nova-ui-kit'
+import { useCallback, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { APP_ROUTES } from 'utils/constants'
 import { getAppRoute } from 'utils/getAppRoute'
@@ -14,39 +15,43 @@ const useOccurrenceNavigation = (occurrences?: Occurrence[]) => {
   const nextId =
     currentIndex !== undefined ? occurrences?.[currentIndex + 1]?.id : undefined
 
+  const goToPrev = useCallback(() => {
+    if (!prevId) {
+      return
+    }
+
+    navigate(
+      getAppRoute({
+        to: APP_ROUTES.OCCURRENCE_DETAILS({
+          projectId: projectId as string,
+          occurrenceId: prevId,
+        }),
+        keepSearchParams: true,
+      })
+    )
+  }, [nextId])
+
+  const goToNext = useCallback(() => {
+    if (!nextId) {
+      return
+    }
+
+    navigate(
+      getAppRoute({
+        to: APP_ROUTES.OCCURRENCE_DETAILS({
+          projectId: projectId as string,
+          occurrenceId: nextId,
+        }),
+        keepSearchParams: true,
+      })
+    )
+  }, [nextId])
+
   return {
     prevId,
     nextId,
-    onPrev: () => {
-      if (!prevId) {
-        return
-      }
-
-      navigate(
-        getAppRoute({
-          to: APP_ROUTES.OCCURRENCE_DETAILS({
-            projectId: projectId as string,
-            occurrenceId: prevId,
-          }),
-          keepSearchParams: true,
-        })
-      )
-    },
-    onNext: () => {
-      if (!nextId) {
-        return
-      }
-
-      navigate(
-        getAppRoute({
-          to: APP_ROUTES.OCCURRENCE_DETAILS({
-            projectId: projectId as string,
-            occurrenceId: nextId,
-          }),
-          keepSearchParams: true,
-        })
-      )
-    },
+    goToPrev,
+    goToNext,
   }
 }
 
@@ -55,15 +60,32 @@ export const OccurrenceNavigation = ({
 }: {
   occurrences?: Occurrence[]
 }) => {
-  const { prevId, nextId, onPrev, onNext } =
+  const { prevId, nextId, goToPrev, goToNext } =
     useOccurrenceNavigation(occurrences)
+
+  // Listen to key down events
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') {
+        e.preventDefault()
+        goToPrev()
+      } else if (e.key === 'ArrowRight') {
+        e.preventDefault()
+        goToNext()
+      }
+    }
+
+    document.addEventListener('keydown', onKeyDown)
+
+    return () => document.removeEventListener('keydown', onKeyDown)
+  }, [goToPrev, goToNext])
 
   return (
     <>
       <Button
         className="absolute top-[50%] -left-8 -translate-x-full -translate-y-1/2 z-50"
         disabled={!prevId}
-        onClick={onPrev}
+        onClick={goToPrev}
         size="icon"
         variant="outline"
       >
@@ -72,7 +94,7 @@ export const OccurrenceNavigation = ({
       <Button
         className="absolute top-[50%] -right-8 translate-x-full -translate-y-1/2 z-50"
         disabled={!nextId}
-        onClick={onNext}
+        onClick={goToNext}
         size="icon"
         variant="outline"
       >
