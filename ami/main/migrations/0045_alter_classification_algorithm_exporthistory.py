bb745f6661f58c586acb3909d2d7d54baa49d2de
mohamedelabbas1996 <hack1996man@gmail.com>
2025-02-17 03:19:33 -0500

    fixed migration conflict

--- ami/main/migrations/0045_alter_classification_algorithm_exporthistory.py
+++ ami/main/migrations/0045_alter_classification_algorithm_exporthistory.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("ml", "0017_alter_algorithm_unique_together"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("main", "0044_merge_20250124_2333"),
     ]
