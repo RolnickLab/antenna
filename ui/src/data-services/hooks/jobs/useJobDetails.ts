efdc55246b3006441ba7d336391dda881dee9939
Anna Viklund <annamariaviklund@gmail.com>
2025-02-27 11:11:23 +0100

    chore: make REFETCH_INTERVAL a shared constant

--- ui/src/data-services/hooks/jobs/useJobDetails.ts
+++ ui/src/data-services/hooks/jobs/useJobDetails.ts
@@ -1,10 +1,8 @@
-import { API_ROUTES, API_URL } from 'data-services/constants'
+import { API_ROUTES, API_URL, REFETCH_INTERVAL } from 'data-services/constants'
 import { ServerJob } from 'data-services/models/job'
 import { JobDetails, ServerJobDetails } from 'data-services/models/job-details'
 import { useMemo } from 'react'
 import { useAuthorizedQuery } from '../auth/useAuthorizedQuery'
-
-const REFETCH_INTERVAL = 10000 // Refetch every 10 second
 
 const convertServerRecord = (record: ServerJob) => new JobDetails(record)
 
