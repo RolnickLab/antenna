834dd8ff517e99614da3563e738f884e3791805f
Anna Viklund <annamariaviklund@gmail.com>
2025-03-12 10:43:11 +0100

    fix: update anchor prefix from chapter->section

--- ui/src/components/info-page/code-of-conduct-page/code-of-conduct-page.tsx
+++ ui/src/components/info-page/code-of-conduct-page/code-of-conduct-page.tsx
@@ -2,5 +2,5 @@ import { InfoPage } from '../info-page'
 import markdown from './code-of-conduct.md'
 
 export const CodeOfConductPage = () => (
-  <InfoPage anchorPrefix="chapter" markdown={markdown} />
+  <InfoPage anchorPrefix="section" markdown={markdown} />
 )
