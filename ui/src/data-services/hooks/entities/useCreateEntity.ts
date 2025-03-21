6b49f5b27d95262d4dcf02e1b4cf99911bb123e8
Anna Viklund <annamariaviklund@gmail.com>
2025-03-21 11:29:07 +0100

    feat: make it possible to register new exports

--- ui/src/data-services/hooks/entities/useCreateEntity.ts
+++ ui/src/data-services/hooks/entities/useCreateEntity.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
-import { API_URL, SUCCESS_TIMEOUT } from 'data-services/constants'
+import { API_ROUTES, API_URL, SUCCESS_TIMEOUT } from 'data-services/constants'
 import { getAuthHeader } from 'data-services/utils'
 import { useUser } from 'utils/user/userContext'
 import { EntityFieldValues } from './types'
@@ -13,7 +13,9 @@ export const useCreateEntity = (collection: string, onSuccess?: () => void) => {
   const { mutateAsync, isLoading, isSuccess, reset, error } = useMutation({
     mutationFn: (fieldValues: EntityFieldValues) =>
       axios.post(
-        `${API_URL}/${collection}/`,
+        collection === API_ROUTES.EXPORTS
+          ? `${API_URL}/${collection}/?project_id=${fieldValues.projectId}`
+          : `${API_URL}/${collection}/`, // TODO: Skip this special handling when API is updated
         convertToServerFieldValues(fieldValues),
         {
           headers: getAuthHeader(user),
