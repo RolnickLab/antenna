cfdc4d7a723496ea6243fe4bcc0298d80b75e374
Michael Bunsen <notbot@gmail.com>
2025-04-01 16:00:27 -0700

    fix: fallback to plain `filters` if `filters_display` not available

--- ui/src/data-services/models/export.ts
+++ ui/src/data-services/models/export.ts
@@ -40,9 +40,9 @@ export class Export extends Entity {
   get fileSizeLabel(): string | undefined {
     return this._data.file_size_display
   }
-
   get filtersLabels(): string[] {
-    return Object.entries(this._data.filters_display).map(([key, _filter]) => {
+    const filtersObj = this._data.filters_display || this._data.filters || {}
+    return Object.entries(filtersObj).map(([key, _filter]) => {
       const filter = _filter as { id: number; name: string }
 
       return `${snakeCaseToSentenceCase(key)}: ${filter.id}`
