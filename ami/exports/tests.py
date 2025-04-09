f0c0b149be8d07a72192cc9751183c1fe4661808
Michael Bunsen <notbot@gmail.com>
2025-04-08 22:45:31 -0700

    fix: update export tests so num occurrences is greater than zero

--- ami/exports/tests.py
+++ ami/exports/tests.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import logging
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
@@ -62,14 +63,20 @@ class DataExportTest(TestCase):
 
     def _create_collection(self):
         """Create a SourceImageCollection from deployment captures."""
-        images = self.deployment.captures.all()
+        images = self.project.captures.all()
+        # Use only half of the images for the collection
+        collection_images = images[: images.count() // 2]
+
+        # Ensure collection images are fewer than total images
+        self.assertGreater(len(collection_images), 0, "No collection images to test exports.")
+        self.assertLess(len(collection_images), images.count(), "Collection images should be fewer than total images.")
 
         # Create the collection
         collection = SourceImageCollection.objects.create(
             name="Test Manual Source Image Collection",
             project=self.project,
             method="manual",
-            kwargs={"image_ids": [image.pk for image in images]},
+            kwargs={"image_ids": [image.pk for image in collection_images]},
         )
         collection.save()
 
@@ -100,24 +107,43 @@ class DataExportTest(TestCase):
         with default_storage.open(file_path, "r") as f:
             if format_type == "occurrences_simple_csv":
                 self.validate_csv_records(f)
-            elif format_type == "occurrences_simple_json":
+            elif format_type == "occurrences_api_json":
                 self.validate_json_records(f)
 
         # Clean up the exported file after the test
         default_storage.delete(file_path)
 
+    def test_export_record_count(self):
+        """Test record count in the exported file."""
+        for format_type in self.export_formats:
+            with self.subTest(format=format_type):
+                self.run_and_validate_export(format_type)
+
+    def validate_record_count(self, record_count):
+        """Validate record count in the exported file."""
+        collection_count = (
+            Occurrence.objects.valid()  # type: ignore[union-attr] # Custom queryset method
+            .filter(detections__source_image__collections=self.collection)
+            .distinct()
+            .count()
+        )
+        total_count = Occurrence.objects.valid().filter(project=self.project).count()  # type: ignore[union-attr]
+
+        logger.debug(f"Exported: {record_count}, # in Collection: {collection_count}, # in Project: {total_count}")
+        self.assertGreater(record_count, 0, "Record count should be greater than zero.")
+        self.assertLess(record_count, total_count, "Record count should be less than total occurrences.")
+        self.assertEqual(record_count, collection_count, "Record count does not match expected count.")
+
     def validate_csv_records(self, file):
         """Validate record count in CSV."""
         csv_reader = csv.DictReader(file)
         row_count = sum(1 for row in csv_reader)
-        expected_count = Occurrence.objects.filter(detections__source_image__collections=self.collection).count()
-        self.assertEqual(row_count, expected_count)
+        self.validate_record_count(row_count)
 
     def validate_json_records(self, file):
         """Validate record count in JSON."""
         data = json.load(file)
-        expected_count = Occurrence.objects.filter(detections__source_image__collections=self.collection).count()
-        self.assertEqual(len(data), expected_count)
+        self.validate_record_count(len(data))
 
     def test_csv_export_record_count(self):
         """Test CSV export record count."""
@@ -125,4 +151,4 @@ class DataExportTest(TestCase):
 
     def test_json_export_record_count(self):
         """Test JSON export record count."""
-        self.run_and_validate_export("occurrences_simple_json")
+        self.run_and_validate_export("occurrences_api_json")
