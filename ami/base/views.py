45e82869261df91f96ee2c08ca85a1ea499a8220
Michael Bunsen <notbot@gmail.com>
2025-05-23 00:11:22 -0700

    fix: active project validation

--- ami/base/views.py
+++ ami/base/views.py
@@ -29,11 +29,10 @@ class ProjectMixin:
 
         # If not in URL, try query parameters
         if not project_id:
-            if self.require_project:
-                project_id = SingleParamSerializer[int].clean(
-                    param_name="project_id",
-                    field=serializers.IntegerField(required=self.require_project, min_value=0),
-                    data=self.request.query_params,
-                )
+            project_id = SingleParamSerializer[int].clean(
+                param_name="project_id",
+                field=serializers.IntegerField(required=self.require_project, min_value=0),
+                data=self.request.query_params,
+            )
 
         return get_object_or_404(Project, id=project_id) if project_id else None
