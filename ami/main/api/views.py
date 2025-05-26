c746c933083507dff60671e4a5341e0d05f54ce7
Michael Bunsen <notbot@gmail.com>
2025-05-26 14:04:05 -0700

    fix: ensure all occurrences in list view have a determination

--- ami/main/api/views.py
+++ ami/main/api/views.py
@@ -1118,7 +1118,9 @@ class OccurrenceViewSet(DefaultViewSet, ProjectMixin):
         qs = qs.with_detections_count().with_timestamps()  # type: ignore
         qs = qs.with_identifications()  # type: ignore
 
-        if self.action != "list":
+        if self.action == "list":
+            qs = qs.has_determination()  # type: ignore
+        else:
             qs = qs.prefetch_related(
                 Prefetch(
                     "detections", queryset=Detection.objects.order_by("-timestamp").select_related("source_image")
