48fab11e188eb7b0885160fa7e72c9c0964de80a
Anna Viklund <annamariaviklund@gmail.com>
2025-08-28 10:41:00 +0200

    feat: setup default filters form

--- ui/src/data-services/hooks/projects/useProjectDetails.ts
+++ ui/src/data-services/hooks/projects/useProjectDetails.ts
@@ -1,24 +1,27 @@
 import { API_ROUTES, API_URL } from 'data-services/constants'
-import { Project, ServerProject } from 'data-services/models/project'
+import { ServerProject } from 'data-services/models/project'
+import { ProjectDetails } from 'data-services/models/project-details'
 import { useMemo } from 'react'
 import { useAuthorizedQuery } from '../auth/useAuthorizedQuery'
 
-const convertServerRecord = (record: ServerProject) => new Project(record)
+const convertServerRecord = (record: ServerProject) =>
+  new ProjectDetails(record)
 
 export const useProjectDetails = (
   projectId: string,
   useInternalCache?: boolean
 ): {
-  project?: Project
+  project?: ProjectDetails
   isLoading: boolean
   isFetching: boolean
   error?: unknown
 } => {
-  const { data, isLoading, isFetching, error } = useAuthorizedQuery<Project>({
-    queryKey: [API_ROUTES.PROJECTS, projectId],
-    url: `${API_URL}/${API_ROUTES.PROJECTS}/${projectId}/`,
-    staleTime: useInternalCache ? Infinity : undefined,
-  })
+  const { data, isLoading, isFetching, error } =
+    useAuthorizedQuery<ProjectDetails>({
+      queryKey: [API_ROUTES.PROJECTS, projectId],
+      url: `${API_URL}/${API_ROUTES.PROJECTS}/${projectId}/`,
+      staleTime: useInternalCache ? Infinity : undefined,
+    })
 
   const project = useMemo(
     () => (data ? convertServerRecord(data) : undefined),
--- ui/src/pages/project/default-filters/default-filters.tsx
+++ ui/src/pages/project/default-filters/default-filters.tsx
@@ -1,30 +1,30 @@
 import classNames from 'classnames'
-import { useUpdateProject } from 'data-services/hooks/projects/useUpdateProject'
-import { Project } from 'data-services/models/project'
+import { useUpdateProjectSettings } from 'data-services/hooks/projects/useUpdateProjectSettings'
+import { ProjectDetails } from 'data-services/models/project-details'
 import styles from 'design-system/components/dialog/dialog.module.scss'
-import { DeleteProjectDialog } from 'pages/project-details/delete-project-dialog'
-import { ProjectDetailsForm } from 'pages/project-details/project-details-form'
+import { DefaultFiltersForm } from 'pages/project-details/default-filters-form'
 import { useEffect } from 'react'
 import { useNavigate, useOutletContext } from 'react-router-dom'
 import { APP_ROUTES } from 'utils/constants'
 import { STRING, translate } from 'utils/language'
 
-export const General = () => {
+export const DefaultFilters = () => {
   const navigate = useNavigate()
   const { project } = useOutletContext<{
-    project: Project
+    project: ProjectDetails
   }>()
-  const { updateProject, isLoading, isSuccess, error } = useUpdateProject(
-    project.id
-  )
+  const { updateProjectSettings, isLoading, isSuccess, error } =
+    useUpdateProjectSettings(project.id)
+
+  const canView = project.canUpdate && project.featureFlags.default_filters
 
   useEffect(() => {
-    if (!project.canUpdate) {
+    if (!canView) {
       navigate(APP_ROUTES.PROJECT_DETAILS({ projectId: project.id }))
     }
-  }, [project.canUpdate])
+  }, [canView])
 
-  if (!project.canUpdate) {
+  if (!canView) {
     return null
   }
 
@@ -33,16 +33,15 @@ export const General = () => {
       <div className="bg-background border border-border rounded-md overflow-hidden">
         <div className={classNames(styles.dialogHeader, 'bg-background')}>
           <h1 className={styles.dialogTitle}>
-            {translate(STRING.NAV_ITEM_GENERAL)}
+            {translate(STRING.NAV_ITEM_DEFAULT_FILTERS)}
           </h1>
-          {project.canDelete && <DeleteProjectDialog id={project.id} />}
         </div>
         <div>
-          <ProjectDetailsForm
+          <DefaultFiltersForm
             error={error}
             isLoading={isLoading}
             isSuccess={isSuccess}
-            onSubmit={(data) => updateProject(data)}
+            onSubmit={(data) => updateProjectSettings(data)}
             project={project}
           />
         </div>
