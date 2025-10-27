9840883acf1a44c835dc7d512e2b9813dfa0c247
Anna Viklund <annamariaviklund@gmail.com>
2025-10-27 11:45:50 +0100

    feat: add multi select button to occurrence gallery view

--- ui/src/pages/occurrences/occurrence-gallery.tsx
+++ ui/src/pages/occurrences/occurrence-gallery.tsx
@@ -60,127 +60,168 @@ export const OccurrenceGallery = ({
   }
 
   return (
-    <div className={classNames(galleryStyles.gallery, galleryStyles.large)}>
-      {items.map((item) => {
-        const detailsRoute = getAppRoute({
-          to: APP_ROUTES.OCCURRENCE_DETAILS({
-            projectId: projectId as string,
-            occurrenceId: item.id,
-          }),
-          keepSearchParams: true,
-        })
-        const image = item.images[0]
-        const canUpdate = item.userPermissions.includes(UserPermission.Update)
-        const agreed = userInfo ? item.userAgreed(userInfo.id) : false
-        const checked = selectedItems.includes(item.id)
-        const onCheckedToggle = () => {
-          onSelectedItemsChange?.(
-            !checked
-              ? [...selectedItems, item.id]
-              : selectedItems.filter((id) => id !== item.id)
-          )
-        }
+    <div className="pt-4">
+      <MultiSelectButton
+        items={items}
+        selectedItems={selectedItems}
+        onSelectedItemsChange={onSelectedItemsChange}
+      />
+      <div className={classNames(galleryStyles.gallery)}>
+        {items.map((item) => {
+          const detailsRoute = getAppRoute({
+            to: APP_ROUTES.OCCURRENCE_DETAILS({
+              projectId: projectId as string,
+              occurrenceId: item.id,
+            }),
+            keepSearchParams: true,
+          })
+          const image = item.images[0]
+          const canUpdate = item.userPermissions.includes(UserPermission.Update)
+          const agreed = userInfo ? item.userAgreed(userInfo.id) : false
+          const checked = selectedItems.includes(item.id)
+          const onCheckedToggle = () => {
+            onSelectedItemsChange?.(
+              !checked
+                ? [...selectedItems, item.id]
+                : selectedItems.filter((id) => id !== item.id)
+            )
+          }
 
-        return (
-          <div
-            key={item.id}
-            className="flex flex-col bg-background border border-border rounded-lg overflow-hidden"
-          >
-            <div className="aspect-square border-b border-border group relative">
-              {isSelecting ? (
-                <div
-                  className="w-full h-full relative cursor-pointer"
-                  onClick={onCheckedToggle}
-                >
-                  <img src={image.src} className={cardStyles.image} />
-                </div>
-              ) : (
-                <Link className="w-full h-full relative" to={detailsRoute}>
-                  <img src={image.src} className={cardStyles.image} />
-                </Link>
-              )}
-              {selectable ? (
-                <div
-                  className={classNames(
-                    'absolute top-2 left-2 group-hover:visible',
-                    { invisible: !isSelecting }
-                  )}
-                >
-                  <Button
-                    className={classNames('hover:text-opacity-100', {
-                      'text-opacity-0': !checked,
-                      'group-hover:text-opacity-100': isSelecting,
-                    })}
+          return (
+            <div
+              key={item.id}
+              className="flex flex-col bg-background border border-border rounded-lg overflow-hidden"
+            >
+              <div className="aspect-square border-b border-border group relative">
+                {isSelecting ? (
+                  <div
+                    className="w-full h-full relative cursor-pointer"
                     onClick={onCheckedToggle}
-                    size="icon"
-                    variant={checked ? 'default' : 'outline'}
                   >
-                    <CheckIcon className="w-4 h-4" />
-                  </Button>
-                </div>
-              ) : null}
-            </div>
-            <div className="grow flex flex-col justify-between gap-2 p-4">
-              <div className="flex items-center gap-2">
-                <Link to={detailsRoute}>
-                  <span
+                    <img src={image.src} className={cardStyles.image} />
+                  </div>
+                ) : (
+                  <Link className="w-full h-full relative" to={detailsRoute}>
+                    <img src={image.src} className={cardStyles.image} />
+                  </Link>
+                )}
+                {selectable ? (
+                  <div
                     className={classNames(
-                      'body-base font-medium text-primary',
-                      {
-                        italic: isGenusOrBelow(item.determinationTaxon),
-                      }
+                      'absolute top-2 left-2 group-hover:visible',
+                      { invisible: !isSelecting }
                     )}
                   >
-                    {item.determinationTaxon.name}
-                  </span>
-                </Link>
-              </div>
-              <div className="flex flex-wrap items-center justify-start gap-2">
-                {item.determinationScore !== undefined ? (
-                  <BasicTooltip
-                    content={
-                      item.determinationVerified
-                        ? translate(STRING.VERIFIED_BY, {
-                            name: item.determinationVerifiedBy?.name,
-                          })
-                        : translate(STRING.MACHINE_PREDICTION_SCORE, {
-                            score: `${item.determinationScore}`,
-                          })
-                    }
-                  >
-                    <IdentificationScore
-                      confirmed={item.determinationVerified}
-                      confidenceScore={item.determinationScore}
-                    />
-                  </BasicTooltip>
+                    <Button
+                      className={classNames('hover:text-opacity-100', {
+                        'text-opacity-0': !checked,
+                        'group-hover:text-opacity-100': isSelecting,
+                      })}
+                      onClick={onCheckedToggle}
+                      size="icon"
+                      variant={checked ? 'default' : 'outline'}
+                    >
+                      <CheckIcon className="w-4 h-4" />
+                    </Button>
+                  </div>
                 ) : null}
-                {!isSelecting && canUpdate && (
-                  <>
-                    <Agree
-                      agreed={agreed}
-                      agreeWith={{
-                        identificationId: item.determinationIdentificationId,
-                        predictionId: item.determinationPredictionId,
-                      }}
-                      applied
-                      compact
-                      occurrenceId={item.id}
-                      taxonId={item.determinationTaxon.id}
-                    />
-                    <SuggestIdPopover occurrenceIds={[item.id]} />
-                    <IdQuickActions
-                      occurrenceIds={[item.id]}
-                      occurrenceTaxa={[item.determinationTaxon]}
-                      zIndex={1}
-                    />
-                  </>
-                )}
+              </div>
+              <div className="grow flex flex-col justify-between gap-2 p-4">
+                <div className="flex items-center gap-2">
+                  <Link to={detailsRoute}>
+                    <span
+                      className={classNames(
+                        'body-base font-medium text-primary',
+                        {
+                          italic: isGenusOrBelow(item.determinationTaxon),
+                        }
+                      )}
+                    >
+                      {item.determinationTaxon.name}
+                    </span>
+                  </Link>
+                </div>
+                <div className="flex flex-wrap items-center justify-start gap-2">
+                  {item.determinationScore !== undefined ? (
+                    <BasicTooltip
+                      content={
+                        item.determinationVerified
+                          ? translate(STRING.VERIFIED_BY, {
+                              name: item.determinationVerifiedBy?.name,
+                            })
+                          : translate(STRING.MACHINE_PREDICTION_SCORE, {
+                              score: `${item.determinationScore}`,
+                            })
+                      }
+                    >
+                      <IdentificationScore
+                        confirmed={item.determinationVerified}
+                        confidenceScore={item.determinationScore}
+                      />
+                    </BasicTooltip>
+                  ) : null}
+                  {!isSelecting && canUpdate && (
+                    <>
+                      <Agree
+                        agreed={agreed}
+                        agreeWith={{
+                          identificationId: item.determinationIdentificationId,
+                          predictionId: item.determinationPredictionId,
+                        }}
+                        applied
+                        compact
+                        occurrenceId={item.id}
+                        taxonId={item.determinationTaxon.id}
+                      />
+                      <SuggestIdPopover occurrenceIds={[item.id]} />
+                      <IdQuickActions
+                        occurrenceIds={[item.id]}
+                        occurrenceTaxa={[item.determinationTaxon]}
+                        zIndex={1}
+                      />
+                    </>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        )
-      })}
-      {!isLoading && items.length === 0 && <EmptyState />}
+          )
+        })}
+        {!isLoading && items.length === 0 && <EmptyState />}
+      </div>
     </div>
+  )
+}
+
+interface MultiSelectButtonProps {
+  items: Occurrence[]
+  selectedItems?: string[]
+  onSelectedItemsChange?: (selectedItems: string[]) => void
+}
+
+const MultiSelectButton = ({
+  items = [],
+  selectedItems,
+  onSelectedItemsChange,
+}: MultiSelectButtonProps) => {
+  const deselectAll = () => onSelectedItemsChange?.([])
+  const selectAll = () => onSelectedItemsChange?.(items.map((item) => item.id))
+
+  const allSelected =
+    selectedItems?.length && selectedItems.length === items.length
+
+  return (
+    <Button
+      onClick={() => {
+        if (allSelected) {
+          deselectAll()
+        } else {
+          selectAll()
+        }
+      }}
+      size="small"
+      variant="outline"
+    >
+      <span>{allSelected ? 'Deselect all' : 'Select all'}</span>
+    </Button>
   )
 }
