439249a1e460a7d69372ef4095555192839286c4
Anna Viklund <annamariaviklund@gmail.com>
2025-11-24 16:25:27 +0100

    feat: prepare dialog for adding members

--- ui/src/pages/project/team/add-member-dialog.tsx
+++ ui/src/pages/project/team/add-member-dialog.tsx
@@ -4,35 +4,42 @@ import {
   FormSection,
 } from 'components/form/layout/layout'
 import { SUCCESS_TIMEOUT } from 'data-services/constants'
-import { useUpdateMember } from 'data-services/hooks/team/useUpdateMember'
-import { Member } from 'data-services/models/member'
+import { useAddMember } from 'data-services/hooks/team/useAddMember'
 import { SaveButton } from 'design-system/components/button/save-button'
 import * as Dialog from 'design-system/components/dialog/dialog'
 import { InputContent } from 'design-system/components/input/input'
-import { Button } from 'nova-ui-kit'
+import { PlusIcon } from 'lucide-react'
+import { Button, Input } from 'nova-ui-kit'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { STRING, translate } from 'utils/language'
 import { useFormError } from 'utils/useFormError'
 import { RolesPicker } from './roles-picker'
 
-export const ManageAccessDialog = ({ member }: { member: Member }) => {
-  const { projectId } = useParams()
+const DEFAULT_ROLE_ID = 'BasicMember'
+
+export const AddMemberDialog = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const [roleId, setRoleId] = useState<string>(member.role.id)
-  const { updateMember, isLoading, isSuccess, error } = useUpdateMember(
-    member.id
-  )
+  const [email, setEmail] = useState('')
+  const [roleId, setRoleId] = useState(DEFAULT_ROLE_ID)
+  const { isLoading, isSuccess, error } = useAddMember()
   const errorMessage = useFormError({ error })
 
   // Reset form on open state change
-  useEffect(() => setRoleId(member.role.id), [isOpen, member])
+  useEffect(() => {
+    setEmail('')
+    setRoleId(DEFAULT_ROLE_ID)
+  }, [isOpen])
 
   return (
     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
       <Dialog.Trigger>
         <Button size="small" variant="outline">
-          <span>{translate(STRING.MANAGE_ACCESS)}</span>
+          <PlusIcon className="w-4 h-4" />
+          <span>
+            {translate(STRING.ENTITY_ADD, {
+              type: translate(STRING.ENTITY_TYPE_MEMBER),
+            })}
+          </span>
         </Button>
       </Dialog.Trigger>
       <Dialog.Content ariaCloselabel={translate(STRING.CLOSE)} isCompact>
@@ -43,13 +50,19 @@ export const ManageAccessDialog = ({ member }: { member: Member }) => {
           />
         )}
         <FormSection
-          title={translate(STRING.MANAGE_ACCESS)}
-          description={translate(STRING.MANAGE_ACCESS_FOR, {
-            user: member.email,
+          title={translate(STRING.ENTITY_ADD, {
+            type: translate(STRING.ENTITY_TYPE_MEMBER),
           })}
         >
+          <InputContent label={translate(STRING.FIELD_LABEL_EMAIL)}>
+            <Input
+              onChange={(e) => setEmail(e.currentTarget.value)}
+              type="email"
+              value={email}
+            />
+          </InputContent>
           <InputContent label={translate(STRING.FIELD_LABEL_ROLE)}>
-            <RolesPicker value={roleId} onValueChange={setRoleId} />
+            <RolesPicker onValueChange={setRoleId} value={roleId} />
           </InputContent>
         </FormSection>
         <FormActions>
@@ -64,11 +77,6 @@ export const ManageAccessDialog = ({ member }: { member: Member }) => {
             isLoading={isLoading}
             isSuccess={isSuccess}
             onClick={async () => {
-              await updateMember({
-                roleId,
-                projectId: projectId as string,
-              })
-
               setTimeout(() => setIsOpen(false), SUCCESS_TIMEOUT)
             }}
           />
--- ui/src/pages/project/team/team.tsx
+++ ui/src/pages/project/team/team.tsx
@@ -3,14 +3,13 @@ import { ProjectDetails } from 'data-services/models/project-details'
 import { PageHeader } from 'design-system/components/page-header/page-header'
 import { PaginationBar } from 'design-system/components/pagination-bar/pagination-bar'
 import { Table } from 'design-system/components/table/table/table'
-import { PlusIcon } from 'lucide-react'
-import { Button } from 'nova-ui-kit'
 import { useEffect } from 'react'
 import { useNavigate, useOutletContext } from 'react-router-dom'
 import { APP_ROUTES } from 'utils/constants'
 import { STRING, translate } from 'utils/language'
 import { usePagination } from 'utils/usePagination'
 import { useUserInfo } from 'utils/user/userInfoContext'
+import { AddMemberDialog } from './add-member-dialog'
 import { columns } from './team-columns'
 
 export const Team = () => {
@@ -45,14 +44,7 @@ export const Team = () => {
         isLoading={isLoading}
         isFetching={isFetching}
       >
-        <Button size="small" variant="outline">
-          <PlusIcon className="w-4 h-4" />
-          <span>
-            {translate(STRING.ENTITY_ADD, {
-              type: translate(STRING.ENTITY_TYPE_MEMBER),
-            })}
-          </span>
-        </Button>
+        <AddMemberDialog />
       </PageHeader>
       <Table
         columns={columns(userInfo?.id)}
