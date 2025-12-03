35c12c1deb84b7e5177622c2b47d0eac9ca67cd3
Michael Bunsen <notbot@gmail.com>
2025-12-02 20:42:23 -0800

    feat: use helper for pre & post update signals for job status

--- ami/jobs/tasks.py
+++ ami/jobs/tasks.py
@@ -29,14 +29,7 @@ def run_job(self, job_id: int) -> None:
             job.logger.info(f"Finished job {job}")
 
 
-@task_prerun.connect(sender=run_job)
-def pre_update_job_status(sender, task_id, task, **kwargs):
-    # in the prerun signal, set the job status to PENDING
-    update_job_status(sender, task_id, task, "PENDING", **kwargs)
-
-
-@task_postrun.connect(sender=run_job)
-def update_job_status(sender, task_id, task, state: str, retval=None, **kwargs):
+def _update_job_status(sender, task_id, task, state: str, **kwargs):
     from ami.jobs.models import Job
 
     job_id = task.request.kwargs["job_id"]
@@ -52,7 +45,27 @@ def update_job_status(sender, task_id, task, state: str, retval=None, **kwargs):
             logger.error(f"No job found for task {task_id} or job_id {job_id}")
             return
 
-    job.update_status(state)
+    job.update_status(state, save=True)
+
+
+@task_prerun.connect(sender=run_job)
+def pre_update_job_status(sender, task_id, task, **kwargs):
+    from ami.jobs.models import JobState
+
+    # In the prerun signal, always set the job status to PENDING
+    _update_job_status(sender, task_id, task, JobState.PENDING, **kwargs)
+
+
+@task_postrun.connect(sender=run_job)
+def post_update_job_status(sender, task_id, task, state: str, retval=None, **kwargs):
+    from ami.jobs.models import JobState
+
+    if state not in JobState.final_states():
+        # If a job is still in a non-final state after run, something went wrong.
+        # We at least know the job is NOT in a running or waiting state.
+        # Set the status to UNKNOWN.
+        state = JobState.UNKNOWN
+    _update_job_status(sender, task_id, task, state, **kwargs)
 
 
 @task_failure.connect(sender=run_job, retry=False)
