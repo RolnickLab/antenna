d4bbd8a4954a435c1487d406c1bab0a46f32946f
Carlos Garcia Jurado Suarez <carlosgjs@live.com>
2026-02-12 09:34:59 -0800

    CR feedback

--- scripts/debug_tests.sh
+++ scripts/debug_tests.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # small helper to launch the test with debugpy enabled, allowing you to attach a VS Code debugger to the test run
 # See the "Debug Tests" launch configuration in .vscode/launch.json
 
 # e.g.  scripts/debug_tests.sh ami.jobs.test_tasks --keepdb
 docker compose run --rm -p 5680:5680 django \
   python -m debugpy --listen 0.0.0.0:5680 --wait-for-client \
-  manage.py test $*
+  manage.py test "$@"
