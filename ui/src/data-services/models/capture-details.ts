2545a46c241657cc0089e70dbcb577ec5004c9dd
Anna Viklund <annamariaviklund@gmail.com>
2026-04-07 12:01:22 +0200

    fix: return latest jobs instead of oldest jobs

--- ui/src/data-services/models/capture-details.ts
+++ ui/src/data-services/models/capture-details.ts
@@ -28,7 +28,7 @@ export class CaptureDetails extends Capture {
       const time1 = j1.updatedAt?.getTime() ?? 0
       const time2 = j2.updatedAt?.getTime() ?? 0
 
-      return time1 - time2
+      return time2 - time1
     })[0]
   }
 
