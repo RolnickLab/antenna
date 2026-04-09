747830295f0c410f38c0dbb14cc35e0ae3239143
Anna Viklund <annamariaviklund@gmail.com>
2026-04-09 11:03:53 +0200

    fix: cleanup forms

--- ui/src/pages/project-details/delete-project-dialog.tsx
+++ ui/src/pages/project-details/delete-project-dialog.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames'
 import { DeleteForm } from 'components/form/delete-form/delete-form'
 import { useDeleteProject } from 'data-services/hooks/projects/useDeleteProject'
 import * as Dialog from 'design-system/components/dialog/dialog'
@@ -8,7 +7,6 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { APP_ROUTES } from 'utils/constants'
 import { STRING, translate } from 'utils/language'
-import styles from './styles.module.scss'
 
 export const DeleteProjectDialog = ({ id }: { id: string }) => {
   const navigate = useNavigate()
@@ -29,16 +27,14 @@ export const DeleteProjectDialog = ({ id }: { id: string }) => {
         </Button>
       </Dialog.Trigger>
       <Dialog.Content ariaCloselabel={translate(STRING.CLOSE)} isCompact>
-        <div className={classNames(styles.content, styles.compact)}>
-          <DeleteForm
-            error={error}
-            type={translate(STRING.ENTITY_TYPE_PROJECT)}
-            isLoading={isLoading}
-            isSuccess={isSuccess}
-            onCancel={() => setIsOpen(false)}
-            onSubmit={() => deleteProject(id)}
-          />
-        </div>
+        <DeleteForm
+          error={error}
+          type={translate(STRING.ENTITY_TYPE_PROJECT)}
+          isLoading={isLoading}
+          isSuccess={isSuccess}
+          onCancel={() => setIsOpen(false)}
+          onSubmit={() => deleteProject(id)}
+        />
       </Dialog.Content>
     </Dialog.Root>
   )
